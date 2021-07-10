7c940a0e94bafff3ec7ba3eba81d6b0b8e163b03
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2021-07-10 10:35:35 +0530

    Create basic_helpers.py

---
+++ main/helper_func/basic_helpers.py
@@ -0,0 +1,253 @@
+import asyncio
+import logging
+import math
+import os
+import shlex
+import time
+from math import ceil
+from traceback import format_exc
+from typing import Tuple
+
+from pyrogram import Client
+from pyrogram.errors import FloodWait, MessageNotModified
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+    Message,
+)
+
+from Stark.config import Config
+
+import mimetypes
+
+
+def guess_mime_type(file_):
+    """Get Mime Type Of A File From Url / Path"""
+    s = mimetypes.guess_type(file_)
+    if not s[0]:
+        return None
+    else:
+        return s[0]
+
+
+def get_user(message: Message, text: str) -> [int, str, None]:
+    """Get User From Message"""
+    if text is None:
+        asplit = None
+    else:
+        asplit = text.split(" ", 1)
+    user_s = None
+    reason_ = None
+    if message.reply_to_message:
+        user_s = message.reply_to_message.from_user.id
+        reason_ = text if text else None
+    elif asplit is None:
+        return None, None
+    elif len(asplit[0]) > 0:
+        if message.entities:
+            if len(message.entities) == 1:
+                required_entity = message.entities[0]
+                if required_entity.type == "text_mention":
+                    user_s = int(required_entity.user.id)
+                else:
+                    user_s = int(asplit[0]) if asplit[0].isdigit() else asplit[0]
+        else:
+            user_s = int(asplit[0]) if asplit[0].isdigit() else asplit[0]
+        if len(asplit) == 2:
+            reason_ = asplit[1]
+    return user_s, reason_
+    
+
+async def is_admin_or_owner(message, user_id) -> bool:
+    """Check If A User Is Creator Or Admin Of The Current Group"""
+    if message.chat.type in ["private", "bot"]:
+        # You Are Boss Of Pvt Chats.
+        return True
+    user_s = await message.chat.get_member(int(user_id))
+    if user_s.status in ("creator", "administrator"):
+        return True
+    return False
+
+
+def get_readable_time(seconds: int) -> int:
+    """Get Time So That Human Can ReadIt"""
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+    
+
+def inline_wrapper(func):
+    async def wrapper(client, inline_query):
+        users = await get_all_pros()
+        if inline_query.from_user.id not in users:
+            await client.answer_inline_query(
+                inline_query.id,
+                cache_time=1,
+                results=[
+                    (
+                        InlineQueryResultArticle(
+                            title="Sorry, Friend You Can't Use Me!",
+                            input_message_content=InputTextMessageContent(
+                                "**Hai!** Well, I am Not For You, I Only Work For My Master. Why Don't You Deploy Your Own @FridayOT ?"
+                            ),
+                        )
+                    )
+                ],
+            )
+        else:
+            await func(client, inline_query)
+
+    return wrapper
+
+
+def humanbytes(size):
+    """Convert Bytes To Bytes So That Human Can Read It"""
+    if not size:
+        return ""
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+
+
+def time_formatter(milliseconds: int) -> str:
+    """Time Formatter"""
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = (
+        ((str(days) + " day(s), ") if days else "")
+        + ((str(hours) + " hour(s), ") if hours else "")
+        + ((str(minutes) + " minute(s), ") if minutes else "")
+        + ((str(seconds) + " second(s), ") if seconds else "")
+        + ((str(milliseconds) + " millisecond(s), ") if milliseconds else "")
+    )
+    return tmp[:-2]
+
+
+async def progress(current, total, message, start, type_of_ps, file_name=None):
+    """Progress Bar For Showing Progress While Uploading / Downloading File - Normal"""
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        if elapsed_time == 0:
+            return
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+        progress_str = "{0}{1} {2}%\n".format(
+            "".join(["▰" for i in range(math.floor(percentage / 10))]),
+            "".join(["▱" for i in range(10 - math.floor(percentage / 10))]),
+            round(percentage, 2),
+        )
+        tmp = progress_str + "{0} of {1}\nETA: {2}".format(
+            humanbytes(current), humanbytes(total), time_formatter(estimated_total_time)
+        )
+        if file_name:
+            try:
+                await message.edit(
+                    "{}\n**File Name:** `{}`\n{}".format(type_of_ps, file_name, tmp)
+                )
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+            except MessageNotModified:
+                pass
+        else:
+            try:
+                await message.edit("{}\n{}".format(type_of_ps, tmp))
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+            except MessageNotModified:
+                pass
+
+
+def get_text(message: Message) -> [None, str]:
+    """Extract Text From Commands"""
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
+        return None
+
+
+async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
+    """ run command in terminal """
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return (
+        stdout.decode("utf-8", "replace").strip(),
+        stderr.decode("utf-8", "replace").strip(),
+        process.returncode,
+        process.pid,
+    )
+
+
+async def edit_or_send_as_file(
+    text: str,
+    message: Message,
+    client: Client,
+    caption: str = "`Result!`",
+    file_name: str = "result",
+    parse_mode="md",
+):
+    """Send As File If Len Of Text Exceeds Tg Limit Else Edit Message"""
+    if not text:
+        await message.edit("`Wait, What?`")
+        return
+    if len(text) > 4567:
+        await message.edit("`OutPut is Too Large, Sending As File!`")
+        file_names = f"{file_name}.text"
+        open(file_names, "w").write(text)
+        await client.send_document(message.chat.id, file_names, caption=caption)
+        await message.delete()
+        if os.path.exists(file_names):
+            os.remove(file_names)
+        return
+    else:
+        return await message.edit(text, parse_mode=parse_mode
+
+
+async def iter_chats(client):
+    """Iter Your All Chats"""
+    chats = []
+    async for dialog in client.iter_dialogs():
+        if dialog.chat.type in ["supergroup", "channel"]:
+            chats.append(dialog.chat.id)
+    return chats
