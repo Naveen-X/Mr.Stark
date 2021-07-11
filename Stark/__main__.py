21d31919d62a6d0e050c71759f57eee5273336dc
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2021-07-11 14:51:08 +0530

    Update __main__.py

--- Stark/__main__.py
+++ Stark/__main__.py
@@ -1,1 +1,33 @@
+import os
 
+import pyrogram
+
+import logging
+
+from Stark.config import Config
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+
+                    level=logging.WARNING)
+
+if __name__ == "__main__" :
+
+    print("Starting Bot...")
+
+    plugins = dict(root="Stark/Plugins")
+
+    app = pyrogram.Client(
+
+        "Mr.stark",
+
+        bot_token=Config.BOT_TOKEN,
+
+        api_id=Config.API_ID,
+
+        api_hash=Config.API_HASH,
+
+        plugins=plugins
+
+    )
+
+    app.run()
