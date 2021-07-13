caed29d510de6b816e1f8755686e4cafe0cdeeb6
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2021-07-13 14:11:48 +0530

    Update startup_helpers.py

--- main/helper_func/startup_helpers.py
+++ main/helper_func/startup_helpers.py
@@ -12,34 +12,7 @@ from os import environ, execle, path, remove
 import heroku3
 from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
-
-
-
-def load_modules(plugin_name):
-    """Load All Extra Plugins Using ImportLib"""
-        plugin_path = "Stark/Plugins." + plugin_name
-        loader_type = "[USER][XTRA-PLUGINS]"
-        importlib.import_module(plugin_path)
-        logging.info(f"{loader_type} - Loaded : " + str(plugin_name)
-
-
-def plugin_collecter(path):
-    """Collects All Files In A Path And Give Its Name"""
-    if path.startswith("/"):
-        path = path[1:]
-    if path.endswith("/"):
-        pathe = path + "*.py"
-    else:
-        pathe = path + "/*.py"
-    Poppy = glob.glob(pathe)
-    final = []
-    Pop = Poppy
-    for x in Pop:
-        k = ntpath.basename(x)
-        if k.endswith(".py"):
-            lily = k.replace(".py", "")
-            final.append(lily)
-    return final  
+  
 
 async def run_cmd(cmd: str) -> Tuple[str, str, int, int]:
     """Run Commands"""
