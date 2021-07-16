cb3c1d5e5ddff2027023d6fc135656d543d63789
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2021-07-16 18:16:31 +0530

    Update start.py

--- Stark/Plugins/start.py
+++ Stark/Plugins/start.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 @Client.on_message(filters.command(["start"]))
 async def start(bot, message):
     firstname = message.from_user.first_name
-    text=f"__Hello, {firstname} !__\nNice To Meet You, Well I Am A Powerfull Assistant bot For My Master!`. \n__**Made By @Sniper_xd**__"
+    text=f"__Hello, {firstname} !\nNice To Meet You, Well I Am A Powerfull Assistant bot For My Master!`. \nMade By @Sniper_xd__"
     stark="https://telegra.ph//file/17d0306972cdc7350abc3.jpg"
     await bot.send_photo(
             message.chat.id,
@@ -17,7 +17,7 @@ async def start(bot, message):
                     [
                         InlineKeyboardButton(
                             "Dev",
-                            url=f"t.me/Sinper_xd",
+                            url="t.me/Sinper_xd",
                         )
                     ],
                     [
