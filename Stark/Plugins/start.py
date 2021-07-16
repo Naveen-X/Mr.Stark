197b082d37745458c1d27e14c195fc1003d77b2a
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2021-07-16 18:04:16 +0530

    Update start.py

--- Stark/Plugins/start.py
+++ Stark/Plugins/start.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 @Client.on_message(filters.command(["start"]))
 async def start(bot, message):
     firstname = message.from_user.first_name
-    text=f"__Hello, {firstname} !__\nNice To Meet You, Well I Am A Powerfull Assistant bot For My Master!`. \nMade By (Naveen)[t.me/Sniper_xd]"
+    text=f"__Hello, {firstname} !__\nNice To Meet You, Well I Am A Powerfull Assistant bot For My Master!`. \n__**Made By @Sniper_xd**__"
     stark="https://telegra.ph//file/17d0306972cdc7350abc3.jpg"
     await bot.send_photo(
             message.chat.id,
@@ -22,8 +22,8 @@ async def start(bot, message):
                     ],
                     [
                          InlineKeyboardButton(
-                            "Add Me to Group 👥",
-                            url=f"t.me/Mr_StarkBot?startgroup=true"
+                            "Credits",
+                            url=f"https://github.com/DevsExpo/FridayUserbot"
                         )
                     ],
                 ]
