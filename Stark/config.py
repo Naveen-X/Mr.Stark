764ed7b8c89f34ffd976c47784660b11aa3e81bd
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2021-07-16 16:30:47 +0530

    Update config.py

--- Stark/config.py
+++ Stark/config.py
@@ -31,5 +31,7 @@ class Config(object):
     API_ID = int(os.environ.get("API_ID", 1))
     API_HASH = os.environ.get("API_HASH", None)
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+    PRO_USERS = set(int(x) for x in os.environ.get("PRO_USERS", "").split())
+    PRO = list(PRO_USERS)
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
