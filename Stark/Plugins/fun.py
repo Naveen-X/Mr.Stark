afb0ebe1e5aaa72e999e42f76b9a2fef5b953548
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2021-07-24 14:45:57 +0530

    update fun.py

--- Stark/Plugins/fun.py
+++ Stark/Plugins/fun.py
@@ -1,1 +1,26 @@
+from pyrogram import Client, filters
 
+
+@Client.on_message(fildart.command(["dart"]))
+asyync def dart(bot, message):
+  await bot.send_dice(message.chat.id, "🎯")
+
+
+@Client.on_message(filters.command(["dice"]))
+async def dice(bot, message):
+  await bot.send_dice(message.chat.id, "🎲")
+
+
+@Client.on_message(filters.command(["dice"]))
+async def dice(bot, message):
+  await bot.send_dice(message.chat.id, "🎯")
+
+
+@Client.on_message(filters.command(["dice"]))
+async def dice(bot, message):
+  await bot.send_dice(message.chat.id, "🎯")
+
+
+@Client.on_message(filters.command(["dice"]))
+async def dice(bot, message):
+  await bot.send_dice(message.chat.id, "🎯")
