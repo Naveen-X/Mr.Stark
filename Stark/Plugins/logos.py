cbeccbd28a3f79158211effa744392911e71cbee
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2021-07-28 13:46:21 +0530

    update logos.py

--- Stark/Plugins/logos.py
+++ Stark/Plugins/logos.py
@@ -47,6 +47,54 @@ async def black_logo(bot, message):
     await event.delete()
     if os.path.exists(file_name):
         os.remove(file_name)
+        
+        
+
+
+@Client.on_message(filters.command(["slogo"]))
+async def slogo(bot, message):
+  event = await message.reply_text("`Processing`")
+  text = message.text.split(None, 1)[1]
+  if not text:
+    await event.edit("`What logo should i make without text!`")
+    return
+  fpath = glob.glob("resources/Fonts/*")
+  font = random.choice(fpath)
+  img = Image.open("./resources/images/yellow_bg_for_logo.jpg")
+  draw = ImageDraw.Draw(img)
+  font = ImageFont.truetype(font, 220)
+  image_widthz, image_heightz = img.size
+  w, h = draw.textsize(text, font=font)
+  h += int(h * 0.21)
+  draw.text(
+      ((image_widthz - w) / 2, (image_heightz - h) / 2),
+      text,
+      font=font,
+      fill=(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
+  )
+  x = (image_widthz - w) / 2
+  y = (image_heightz - h) / 2
+  await client.send_chat_action(message.chat.id, "upload_photo")
+  draw.text(
+      (x, y), text, font=font, fill="white", stroke_width=60, stroke_fill="black"
+  )
+  fname2 = "LogoBy@FRIDAYOT.png"
+  img.save(fname2, "png")
+  await client.send_chat_action(message.chat.id, "cancel")
+  if message.reply_to_message:
+    await client.send_photo(
+        message.chat.id,
+        photo=fname2,
+        caption="Made Using @Mr_StarkBot",
+        reply_to_message_id=message.reply_to_message.message_id,
+    )
+  else:
+      await client.send_photo(
+          message.chat.id, photo=fname2, caption="Made Using FridayUserBot"
+      )
+  await event.delete()
+  if os.path.exists(fname2):
+    os.remove(fname2) 
 
 
 __help__ = """
