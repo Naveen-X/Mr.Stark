d3ba60470bd724a25611c2f27f03e6322955ba15
Casperteam <84174959+CasperTeam@users.noreply.github.com>
2021-09-06 15:43:43 +0530

    Create inline.py

---
+++ Stark/Plugins/inline.py
@@ -0,0 +1,76 @@
+import asyncio
+from pyrogram.handlers import InlineQueryHandler
+from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client, errors
+from config import Config
+from youtubesearchpython import VideosSearch
+
+USERNAME = Config.BOT_TOKEN
+
+buttons = [
+            [
+                InlineKeyboardButton("🤖contact me in pm 🤖", url="https://t.me/Mr_StarkBot"),
+            ]
+         ]
+
+@Client.on_inline_query()
+async def search(client, query):
+    answers = []
+    if query.query == "hi":
+        answers.append(
+            InlineQueryResultArticle(
+                title="click to contact me in pm",
+                input_message_content=InputTextMessageContent("hi"),
+                reply_markup=InlineKeyboardMarkup(buttons)
+                )
+            )
+        await query.answer(results=answers, cache_time=0)
+        return
+    string = query.query.lower().strip().rstrip()
+    if string == "":
+        await client.answer_inline_query(
+            query.id,
+            results=answers,
+            switch_pm_text=("✍️ Type An Video Name!"),
+            switch_pm_parameter="help",
+            cache_time=0
+        )
+    else:
+        videosSearch = VideosSearch(string.lower(), limit=50)
+        for v in videosSearch.result()["result"]:
+            answers.append(
+                InlineQueryResultArticle(
+                    title=v["title"],
+                    description=("Duration: {} Views: {}").format(
+                        v["duration"],
+                        v["viewCount"]["short"]
+                    ),
+                    input_message_content=InputTextMessageContent(
+                        "Here is your Youtube Link BY @Mr_StarkBot \n\n https://www.youtube.com/watch?v={}".format(
+                            v["id"]
+                        )
+                    ),
+                    thumb_url=v["thumbnails"][0]["url"]
+                )
+            )
+        try:
+            await query.answer(
+                results=answers,
+                cache_time=0
+            )
+        except errors.QueryIdInvalid:
+            await query.answer(
+                results=answers,
+                cache_time=0,
+                switch_pm_text=("❌ No Results Found!"),
+                switch_pm_parameter="",
+            )
+
+
+__handlers__ = [
+    [
+        InlineQueryHandler(
+            search
+        )
+    ]
+]
