beefbf04e6c622c77632f5c95095984578499ed2
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2021-09-26 17:06:37 +0530

    update chatbot.py

--- Stark/Plugins/chatbot.py
+++ Stark/Plugins/chatbot.py
@@ -64,7 +64,7 @@ async def type_and_send(message: Message):
     chat_id = message.chat.id
     user_id = message.from_user.id if message.from_user else 0
     query = message.text.strip()
-    await message.client.send_chat_action(chat_id, "typing")
+    await message.bot.send_chat_action(chat_id, "typing")
     response, _ = await gather(lunaQuery(query, user_id), sleep(3))
     await message.reply_text(response)
     await message._client.send_chat_action(chat_id, "cancel")
