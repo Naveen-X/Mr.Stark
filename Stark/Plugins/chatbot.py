f693a99a09f53a14e555c95bf9bd4a170f9f5d63
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2021-09-26 14:12:09 +0530

    Fix 🔧

--- Stark/Plugins/chatbot.py
+++ Stark/Plugins/chatbot.py
@@ -2,7 +2,11 @@ import os
 import aiohttp
 import aiofiles
 from pyrogram import Client, filters
+from pyrogram.types import Message
+
 from functools import wraps
+from asyncio import gather, sleep
+
 from aiohttp import ClientSession
 from Python_ARQ import ARQ
 
@@ -14,53 +18,76 @@ KEY = "XYYLVQ-EWWNJL-AUJEDP-PXKSGN-ARQ"
 
 arq = ARQ(URL, KEY, session)
 
-active_chats = []
-
 BOT_ID = 1863795995
+chatbot_group = 2
+active_chats_bot = []
+
+
+async def chat_bot_toggle(db, message: Message):
+    status = message.text.split(None, 1)[1].lower()
+    chat_id = message.chat.id
+    if status == "enable":
+        if chat_id not in db:
+            db.append(chat_id)
+            text = "Chatbot Enabled!"
+            return await message.reply_text(text)
+        await message.reply_text("ChatBot Is Already Enabled.")
+    elif status == "disable":
+        if chat_id in db:
+            db.remove(chat_id)
+            return await message.reply_text("Chatbot Disabled!")
+        await message.reply_text("ChatBot Is Already Disabled.")
+    else:
+        await message.reply_text(
+             "**Usage:**\n/chatbot [ENABLE|DISABLE]"
+        )
+
+
 # Enabled | Disable Chatbot
 
 
-@Client.on_message(filters.command(["chatbot"]))
-async def chatbot_status(bot, message):
-    global active_chats
+@Client.on_message(filters.command("chatbot") & ~filters.edited)
+async def chatbot_status(client, message):
     if len(message.command) != 2:
-        await message.reply_text("/chatbot [ON|OFF]")
-        return
-    status = message.text.split(None, 1)[1]
+        return await message.reply_text(
+            "**Usage:**\n/chatbot [ENABLE|DISABLE]"
+        )
+    await chat_bot_toggle(active_chats_bot, message)
+
+
+async def lunaQuery(query: str, user_id: int):
+    luna = await arq.luna(query, user_id)
+    return luna.result
+
+
+async def type_and_send(message: Message):
     chat_id = message.chat.id
-
-    if status == "ON" or status == "on" or status == "On":
-        if chat_id not in active_chats:
-            active_chats.append(chat_id)
-            text = "Chatbot Enabled Reply To Any Message" \
-                   + "Of Mine To Get A Reply"
-            await message.reply_text(text)
-            return
-        await message.reply_text("ChatBot Is Already Enabled.")
-        return
-
-    elif status == "OFF" or status == "off" or status == "Off":
-        if chat_id in active_chats:
-            active_chats.remove(chat_id)
-            await message.reply_text("Chatbot Disabled!")
-            return
-        await message.reply_text("ChatBot Is Already Disabled.")
-        return
-
-    else:
-        await message.reply_text("/chatbot [ON|OFF]")
-
-
-@Client.on_message(filters.text & filters.reply &
-         ~filters.bot & ~filters.via_bot & ~filters.forwarded, group=2)
-async def chatbot_talk(bot, message):
-    if message.chat.id not in active_chats:
-        return
-    user_id = message.from_user.id
-    query = message.text
-    response = (await arq.luna(query, user_id)).result
-    await bot.send_chat_action(message.chat.id, "typing")
+    user_id = message.from_user.id if message.from_user else 0
+    query = message.text.strip()
+    await message.client.send_chat_action(chat_id, "typing")
+    response, _ = await gather(lunaQuery(query, user_id), sleep(3))
     await message.reply_text(response)
+    await message._client.send_chat_action(chat_id, "cancel")
+
+
+@Client.on_message(
+    filters.text
+    & filters.reply
+    & ~filters.bot
+    & ~filters.via_bot
+    & ~filters.forwarded,
+    group=chatbot_group,
+)
+async def chatbot_talk(_, message: Message):
+    if message.chat.id not in active_chats_bot:
+        return
+    if not message.reply_to_message:
+        return
+    if not message.reply_to_message.from_user:
+        return
+    if message.reply_to_message.from_user.id != BOT_ID:
+        return
+    await type_and_send(message)
 
 
 
