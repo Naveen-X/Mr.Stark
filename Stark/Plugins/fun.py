c4b1d48f91beff2061fb18151636c76c0726a247
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2022-07-03 12:23:24 +0530

    update fun.py

--- Stark/Plugins/fun.py
+++ Stark/Plugins/fun.py
@@ -7,14 +7,14 @@ async def meme(bot, message):
     hmm_s = "https://some-random-api.ml/meme"
     r = requests.get(url=hmm_s).json()
     image_s = r["image"]
-    await bot.send_photo(message.chat.id, image_s, reply_to_message_id=message.message_id)
+    await bot.send_photo(message.chat.id, image_s, reply_to_message_id=message.m id)
 
 @Client.on_message(filters.command(["panda"]))
 async def panda(bot, message):
     link = "https://some-random-api.ml/img/panda"
     r = requests.get(url=link).json()
     image_s = r["link"]
-    await bot.send_photo(message.chat.id, image_s, reply_to_message_id=message.message_id)
+    await bot.send_photo(message.chat.id, image_s, reply_to_message_id=message.m id)
 
 
 @Client.on_message(filters.command(["cat"]))
@@ -22,7 +22,7 @@ async def cat(bot, message):
     link = "https://some-random-api.ml/img/cat"
     r = requests.get(url=link).json()
     image_s = r["link"]
-    await bot.send_photo(message.chat.id, image_s, reply_to_message_id=message.message_id)
+    await bot.send_photo(message.chat.id, image_s, reply_to_message_id=message.m id)
 
 
 @Client.on_message(filters.command(["dog"]))
@@ -30,4 +30,4 @@ async def dog(bot, message):
     link = "https://some-random-api.ml/img/dog"
     r = requests.get(url=link).json()
     image_s = r["link"]
-    await bot.send_photo(message.chat.id, image_s, reply_to_message_id=message.message_id)
+    await bot.send_photo(message.chat.id, image_s, reply_to_message_id=message.m id)
