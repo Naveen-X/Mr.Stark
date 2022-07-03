12b4ab1f5fe438c462c98fe7888c17558de2efbe
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2022-07-03 11:50:53 +0530

    Add meme command

--- main/Mr.Stark/Stark/Plugins/Mr.Stark/Stark/Plugins/fun.py
+++ main/Mr.Stark/Stark/Plugins/Mr.Stark/Stark/Plugins/fun.py
@@ -9,3 +9,25 @@ async def meme(bot, message):
     image_s = r["image"]
     await bot.send_photo(message.chat.id, image_s, reply_to_message_id=message.message_id)
 
+@Client.on_message(filters.command(["panda"]))
+async def panda(bot, message):
+    link = "https://some-random-api.ml/img/panda"
+    r = requests.get(url=link).json()
+    image_s = r["link"]
+    await bot.send_photo(message.chat.id, image_s, reply_to_message_id=message.message_id)
+
+
+@Client.on_message(filters.command(["cat"]))
+async def cat(bot, message):
+    link = "https://some-random-api.ml/img/cat"
+    r = requests.get(url=link).json()
+    image_s = r["link"]
+    await bot.send_photo(message.chat.id, image_s, reply_to_message_id=message.message_id)
+
+
+@Client.on_message(filters.command(["dog"]))
+async def dog(bot, message):
+    link = "https://some-random-api.ml/img/dog"
+    r = requests.get(url=link).json()
+    image_s = r["link"]
+    await bot.send_photo(message.chat.id, image_s, reply_to_message_id=message.message_id)
