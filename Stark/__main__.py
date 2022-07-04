c4208728df1fefbf5cb2410e9d22e6688913a16e
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2022-07-04 18:59:08 +0530

    update __main__.py

--- Stark/__main__.py
+++ Stark/__main__.py
@@ -3,6 +3,7 @@ import time
 import pyrogram
 import logging
 
+from pyrogram import idle
 from Stark.config import Config
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
@@ -10,7 +11,15 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 
 if __name__ == "__main__" :
-
+    plugins = dict(root="Stark/Plugins")
+    app = pyrogram.Client(
+        "Mr.stark",
+        bot_token=Config.BOT_TOKEN,
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH,
+        plugins=plugins
+    )
+    app.start()
     print("Starting Assistant...")
     print("""
   __  __             _____ _             _    
@@ -20,13 +29,4 @@ if __name__ == "__main__" :
  | |  | | |     _   ____) | || (_| | |  |   < 
  |_|  |_|_|    (_) |_____/ \__\__,_|_|  |_|\_\
 """)
-
-    plugins = dict(root="Stark/Plugins")
-    app = pyrogram.Client(
-        "Mr.stark",
-        bot_token=Config.BOT_TOKEN,
-        api_id=Config.API_ID,
-        api_hash=Config.API_HASH,
-        plugins=plugins
-    )
-    app.run()
+    idle()
