ab53dd95a4b3f54f48295cb7cd7b1d5aee5255a4
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2022-07-04 20:47:41 +0530

    Rename __main__.py to __main__.py

---
+++ Stark/__main__.py
@@ -0,0 +1,38 @@
+import os
+import time
+import asyncio
+import pyrogram
+import logging
+
+from pyrogram import idle
+from Stark.config import Config
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+async def startup():
+    print("Starting Assistant...")
+    print("""
+  __  __             _____ _             _    
+ |  \/  |           / ____| |           | |   
+ | \  / |_ __      | (___ | |_ __ _ _ __| | __
+ | |\/| | '__|      \___ \| __/ _` | '__| |/ /
+ | |  | | |     _   ____) | || (_| | |  |   < 
+ |_|  |_|_|    (_) |_____/ \__\__,_|_|  |_|\_\
+""")
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(startup())
+
+if __name__ == "__main__" :
+    plugins = dict(root="Stark/Plugins")
+    app = pyrogram.Client(
+        "Mr.stark",
+        bot_token=Config.BOT_TOKEN,
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH,
+        plugins=plugins
+    )
+    app.start()
+with bot:
+    await app.send_message("-1001426113453", "𝑨𝒔𝒔𝒊𝒔𝒕𝒂𝒏𝒕 𝒉𝒂𝒔 𝒃𝒆𝒆𝒏 𝒔𝒕𝒂𝒓𝒕𝒆𝒅 𝒔𝒖𝒄𝒄𝒆𝒔𝒔𝒇𝒖𝒍𝒍𝒚")
