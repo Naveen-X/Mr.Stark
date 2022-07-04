0af53e4d5346aa5241a7a0fef2c7d5a99a3d5abf
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2022-07-04 20:59:48 +0530

    update __main__.py

--- Stark/__main__.py
+++ Stark/__main__.py
@@ -10,9 +10,21 @@ from Stark.config import Config
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
-async def startup():
-    print("Starting Assistant...")
-    print("""
+
+plugins = dict(root="Stark/Plugins")
+app = pyrogram.Client(
+        "Mr.stark",
+        bot_token=Config.BOT_TOKEN,
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH,
+        plugins=plugins
+    )
+
+with app:
+    app.send_message("-1001426113453", "𝑨𝒔𝒔𝒊𝒔𝒕𝒂𝒏𝒕 𝒉𝒂𝒔 𝒃𝒆𝒆𝒏 𝒔𝒕𝒂𝒓𝒕𝒆𝒅 𝒔𝒖𝒄𝒄𝒆𝒔𝒔𝒇𝒖𝒍𝒍𝒚")
+app.start()
+print("Starting Assistant...")
+print("""
   __  __             _____ _             _    
  |  \/  |           / ____| |           | |   
  | \  / |_ __      | (___ | |_ __ _ _ __| | __
@@ -20,19 +32,5 @@ async def startup():
  | |  | | |     _   ____) | || (_| | |  |   < 
  |_|  |_|_|    (_) |_____/ \__\__,_|_|  |_|\_\
 """)
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(startup())
-
-if __name__ == "__main__" :
-    plugins = dict(root="Stark/Plugins")
-    app = pyrogram.Client(
-        "Mr.stark",
-        bot_token=Config.BOT_TOKEN,
-        api_id=Config.API_ID,
-        api_hash=Config.API_HASH,
-        plugins=plugins
-    )
-with app:
-    await app.send_message("-1001426113453", "𝑨𝒔𝒔𝒊𝒔𝒕𝒂𝒏𝒕 𝒉𝒂𝒔 𝒃𝒆𝒆𝒏 𝒔𝒕𝒂𝒓𝒕𝒆𝒅 𝒔𝒖𝒄𝒄𝒆𝒔𝒔𝒇𝒖𝒍𝒍𝒚")
-app.run()
+print("𝑨𝒔𝒔𝒊𝒔𝒕𝒂𝒏𝒕 𝒉𝒂𝒔 𝒃𝒆𝒆𝒏 𝒔𝒕𝒂𝒓𝒕𝒆𝒅 𝒔𝒖𝒄𝒄𝒆𝒔𝒔𝒇𝒖𝒍𝒍𝒚")
+idle()
