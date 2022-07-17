e11d8f41e3f84e443a0ea1d3d747d7a45dd9d723
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2022-07-17 19:24:03 +0530

    update carbon.py

--- Stark/Plugins/carbon.py
+++ Stark/Plugins/carbon.py
@@ -8,9 +8,9 @@ async def carbon(code):
    return url
 
 @Client.on_message(filters.command(["carbon"]))
-async def carbon(bot, message):
+async def make_carbon(bot, message):
     ok = await message.reply_text("`Making Carbon...`")
-    code = message.text
+    code = message.text.split(' ',1)[1]
     if not code:
         if not message.reply_to_message:
            return await ok.edit("`Nothing To Carbonize..`")
@@ -18,16 +18,16 @@ async def carbon(bot, message):
            return await ok.edit("`Nothing To Carbonize...`")
     code = code or message.reply_to_message.text
     
-    carbon = await carbon(code)
+    karbon = await carbon(code)
     cap = f"__Carbonized By {message.from_user.mention}__\n\n__**By @Mr_StatkBot**"
-    await bot.send_document(message.chat.id, carbon, caption=cap)
+    await bot.send_document(message.chat.id, karbon, caption=cap)
     await ok.delete()
 
 
 @Client.on_message(filters.command(["icarbon"]))
-async def image_karb(bot, message):
+async def image_carb(bot, message):
     ok = await message.reply_text("`Making Carbon...`")
-    code = message.text
+    code = message.text.split(' ',1)[1]
     if not code:
         if not message.reply_to_message:
            return await ok.edit("`Nothing To Carbonize..`")
@@ -35,7 +35,7 @@ async def image_karb(bot, message):
            return await ok.edit("`Nothing To Carbonize...`")
     code = code or message.reply_to_message.text
     
-    carbon = await carbon(code)
+    ikarbon = await carbon(code)
     cap = f"__Carbonized By {message.from_user.mention}__\n\n__**By @Mr_StarkBot**"
-    await bot.send_photo(message.chat.id, carbon, caption=cap)
+    await bot.send_photo(message.chat.id, ikarbon, caption=cap)
     await ok.delete()
