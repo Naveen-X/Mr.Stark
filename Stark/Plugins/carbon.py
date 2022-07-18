c25bf4818072a4a824df94ce99260d142793ca4a
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2022-07-18 22:07:47 +0530

    Fix

--- Stark/Plugins/carbon.py
+++ Stark/Plugins/carbon.py
@@ -8,48 +8,50 @@ async def carbon(code):
    return url
 
 @Client.on_message(filters.command(["carbon"]))
-async def make_carbon(bot, message):
-    ok = await message.reply_text("`Making Carbon...`")
+async def make_carbon(bot, message):    
+    ok = await message.reply_text("`Making Carbon...`") 
+    code = None
     if message.reply_to_message:
-        if message.reply_to_message.caption:
-            code = message.reply_to_message
-        elif message.reply_to_message.text:
-            code = message.reply_to_message.text 
-        elif len(message.command) > 1:
-            code = message.text.split(" ",1)[1]
-        if not code:
+          if message.reply_to_message.caption:
+               code = message.reply_to_message
+          elif message.reply_to_message.text:
+              code = message.reply_to_message.text
+    elif len(message.command) > 1:
+        code = message.text.split(" ",1)[1]
+        
+    if not code:
            return await ok.edit("`Nothing To Carbonize...`")
-    input = code or message.reply_to_message.text
-    
-    karbon = await carbon(input)
+           
+    carbon_url = await carbon(code)
     if message.from_user:
         user = message.from_user.mention
     else:
         user = message.sender_chat.title
     cap = f"__Carbonized By {user}__\n\n__**By @Mr_StatkBot**"
-    await bot.send_document(message.chat.id, karbon, caption=cap)
+    await message.reply_document(carbon_url, caption=cap)
     await ok.delete()
 
 
 @Client.on_message(filters.command(["icarbon"]))
-async def image_carb(bot, message):
-    ok = await message.reply_text("`Making Carbon...`")
+async def carbonn(bot, message):    
+    ok = await message.reply_text("Making Carbon...") 
+    code = None
     if message.reply_to_message:
-        if message.reply_to_message.caption:
-            code = message.reply_to_message
-        elif message.reply_to_message.text:
-            code = message.reply_to_message.text 
-        elif len(message.command) > 1:
-            code = message.text.split(" ",1)[1]
-        if not code:
-           return await ok.edit("`Nothing To Carbonize...`")
-    input = code or message.reply_to_message.text
-    
-    karbon = await carbon(input)
+          if message.reply_to_message.caption:
+               code = message.reply_to_message
+          elif message.reply_to_message.text:
+              code = message.reply_to_message.text
+    elif len(message.command) > 1:
+        code = message.text.split(" ",1)[1]
+                  
+    if not code:
+           return await ok.edit("Nothing To Carbonize...")
+           
+    carbon_url = await carbon(code)
     if message.from_user:
         user = message.from_user.mention
     else:
         user = message.sender_chat.title
     cap = f"__Carbonized By {user}__\n\n__**By @Mr_StatkBot**"
-    await bot.send_photo(message.chat.id, ikarbon, caption=cap)
+    await bot.send_photo(message.chat.id, carbon_url, caption=cap)
     await ok.delete()
