514ef87b842a65d2f5a2b136dda1edf4815b5c92
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2022-07-18 20:29:46 +0530

    update logs.py

--- Stark/Plugins/logs.py
+++ Stark/Plugins/logs.py
@@ -1,11 +1,11 @@
 import os
 from pyrogram import Client, filters
 
-@Client.on_message(filters.private & filters.command("log", "logs"))
+@Client.on_message(filters.command(["log", "logs"]))
 async def log_cmd(bot, message):
     processing = await message.reply_text("Processing")
     if os.path.exists("Log.txt"):
-        await message.reply_document("Log.txt",caption="Log.txt")
+        await message.reply_document("log.txt",caption="Logs of Mr.Stark")
         await processing.delete()
     else:
         await processing.edit("File not found",quote=True)
