55ffabf4074f642883cfd3e41276aa74c8ec1d43
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2022-08-03 09:53:28 +0530

    Trying welcome

--- Stark/Plugins/welcome.py
+++ Stark/Plugins/welcome.py
@@ -1,1 +1,14 @@
+from pyrogram import Client, filters, emoji
+from pyrogram.types import Message
 
+@Client.on_message(filters.new_chat_members)
+async def welcome(bot, message: Message):
+  new_members = [f"{u.mention}" for u in message.new_chat_members]
+  text = f"{emoji.SPARKLES} Hey {message.from_user.mention}\nWelcome to {message.chat.title}, Have a Nice Day :")
+  await message.reply_text(text)
+  
+
+@Client.on_message(filters.left_chat_member)
+async def user_left(bot, message: Message):
+  text = "K bye!\n Nice knowing you:("
+  await message.reply_text(text)
