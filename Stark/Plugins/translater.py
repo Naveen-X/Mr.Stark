a80e6f9c7121f1ed6d1a7b7f06ad49e525228b1a
Naveen_xd <85566702+Naveen-X@users.noreply.github.com>
2022-08-14 12:19:14 +0530

    ....

--- Stark/Plugins/translater.py
+++ Stark/Plugins/translater.py
@@ -8,12 +8,11 @@ api = SafoneAPI()
 @Client.on_message(filters.command(["tr", "translate"]))
 async def translate_me(_, message):
   lol = await message.reply_text(f"`Translating please wait!`")
-  lang = message.text.split(None, 1)[1]
-  if lang:
+  lang = message.text.split(None, 1)
+  if len(lang) > 1:
         lang = lang[1]
   else:
         lang = "en"
-        return
   text = message.reply_to_message.text
   if not text:
       await lol.edit("`Reply to a message to translate it`")
