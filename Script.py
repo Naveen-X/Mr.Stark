8cde77dd766847494e8b8e9acdb1e8ab30aae5e5
SaTyEndRa <84174959+S4TyEndRa@users.noreply.github.com>
2022-08-14 20:58:06 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,52 +1,52 @@
 class script(object):
-  TRANSLATE_TXT = """
+  TRANSLATE = """
 <b>Translater </b>
 ➥ /tr <language code> reply to a message - translates the replied message
 """
-  CARBON_TXT = """
+  CARBON = """
 <b>Carbon</b>
 ➥  /carbon <text to be carbonise> or reply to a text message - sends as image
 ➥  /icarbon  <text to be carbonise> or reply to a text message - sends in doc format[no compression]
 """
-  PASTE_TXT = """
+  PASTE = """
 <b>Paste</b>
 ➥ /paste <text to paste or reply to a message or txt file> - Pastes the given text
 """
-  LOGS_TXT = """
+  LOGS = """
 <b>Logs</b>
 ➥ /logs - sends logs
 """
-  SYSTEM_TXT = """
+  SYSTEM = """
 <b>System</b>
 ➥ /ping - shows uptime and speed
 ➥ /alive - For checking the bot is alive or not
 ➥ /restart - Restarts the bot
 """
-  LOGOS_TXT = """
+  LOGOS = """
 <b>Logos</b>
 ➥ /alogo <text> - makes logo with given text
 ➥ /slogo <text> - makes a cool logo with given text
 """
-  WASTED_TXT ="""
+  WASTED ="""
 <b>Wasted</b>
 ➥ /wasted <reply to a image> - try it urself
 """
-  WHOIS_TXT = """
+  WHOIS = """
 <b>Whois</b>
 ➥ /info <username or reply toma user> - gives information about a user
 """
-  WRITE_TXT = """
+  WRITE = """
 <b>Write</b>
 ➥ /write <text or reply to a text> - writes given text on a white paper
 """
-  FUN_TXT = """
+  FUN = """
 <b>System</b>
 ➥ /meme - Sends a meme
 ➥ /panda - Sends a pic of panda
 ➥ /cat - Sends a pic of cat
 ➥ /dog - Sends a pic of dog
 """
-  FTOOLS_TXT = """
+  FTOOLS = """
 <b>FileTools</b>
 ➥ /download  <reply to a file> - downloads given file to local server
 """
