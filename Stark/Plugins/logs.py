5c43ada7264547cb7e7c4754f940345446010a96
Naveen_xd <85566702+Naveen-X@users.noreply.github.com>
2022-08-19 15:48:27 +0530

    Add paste link

--- Stark/Plugins/logs.py
+++ Stark/Plugins/logs.py
@@ -1,11 +1,29 @@
 import os
+from Stark.Plugins.paste import s_paste
 from pyrogram import Client, filters
 
 @Client.on_message(filters.command(["log", "logs"]))
 async def log_cmd(bot, message):
     processing = await message.reply_text("Processing")
     if os.path.exists("log.txt"):
-        await message.reply_document("log.txt",caption="__**Logs of Mr.Stark**__")
+        logs = open("log.txt", r).read()
+        ext = "py"
+        x = await s_paste(logs, ext)
+        link = x["url"]
+        await message.reply_document(
+          "log.txt",
+          caption="__**Logs of Mr.Stark**__",
+          reply_markup=InlineKeyboardMarkup(
+             [
+                 [
+                     InlineKeyboardButton(
+                         text="Logs", 
+                         url=f"{link}",
+                     ),
+                 ],
+             ]
+         )
+        )
         await processing.delete()
     else:
         await processing.edit("`File not found`")
