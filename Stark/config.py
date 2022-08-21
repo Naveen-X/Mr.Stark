2d4321961b21257ab6289b77c578a75e3889db1b
Naveen_xd <85566702+Naveen-X@users.noreply.github.com>
2022-08-21 14:12:58 +0530

    update config.py

--- Stark/config.py
+++ Stark/config.py
@@ -1,31 +1,9 @@
 import os
-
-import heroku3
-
 from dotenv import load_dotenv
 from distutils.util import strtobool
 
 if os.path.exists("local.env"):
     load_dotenv("local.env")
-
-def fetch_heroku_git_url(api_key, app_name):
-    if not api_key:
-        return None
-    if not app_name:
-        return None
-    heroku = heroku3.from_key(api_key)
-    try:
-        heroku_applications = heroku.apps()
-    except:
-        return None
-    heroku_app = None
-    for app in heroku_applications:
-        if app.name == app_name:
-            heroku_app = app
-            break
-    if not heroku_app:
-        return None
-    return heroku_app.git_url.replace("https://", "https://api:" + api_key + "@")
     
 class Config(object):
     API_ID = int(os.environ.get("API_ID", 1))
@@ -33,5 +11,3 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
     PRO_USERS = set(int(x) for x in os.environ.get("PRO_USERS", "").split())
     PRO = list(PRO_USERS)
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
