6454d627d466c7068807c0fa3ca9ba33135573ad
Naveen_xd <naveenxd2580@gmail.com>
2023-04-17 18:43:59 +0530

    update Stark/Plugins/chatgpt.py

---
+++ Stark/Plugins/chatgpt.py
@@ -0,0 +1,24 @@
+import os
+import openai
+import requests 
+from pyrogram import Client, filters
+
+openai.api_key = "YOUR_OPENAI_API_KEY"
+
+def generate_response(user_input):
+    prompt = f"User: {user_input}\nBot:"
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=50,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    message = response.choices[0].text.strip()
+    return message
+
+@Client.on_message(filters.text)
+def chatbot(bot, message):
+    response = generate_response(message.text)
+    bot.send_message(message.chat.id, response, reply_to_message_id=message.id)
