46fb4e6fe6d628de3a2dd6cc5bb9b75450c2443e
Satyendra <satya@devh.in>
2023-04-26 13:45:42 +0530

    Create update.py

---
+++ Stark/Plugins/update.py
@@ -0,0 +1,20 @@
+from pyrogram import Client, filters
+import subprocess
+import os
+import sys
+
+
+@Client.on_message(filters.command("update"))
+def update(client, message):
+    # check if the message is sent by an authorized user
+    if message.from_user.id not in [1246467977, 1089528685]:
+        return
+
+    # fetch the latest changes from the remote repository
+    subprocess.run(["git", "pull"])
+
+    # stop the current process
+    os.kill(os.getpid(), 9)
+
+    # start the bot process again
+    os.execl(sys.executable, sys.executable, *sys.argv)
