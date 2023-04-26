5ca41380ba003c051651a9638486f72144f1579e
Naveen_xd <naveenxd2580@gmail.com>
2023-04-26 14:47:31 +0530

    update Stark/__init__.py

--- Stark/__init__.py
+++ Stark/__init__.py
@@ -1,17 +1,13 @@
 import logging
 from tglogging import TelegramLogHandler
 
-# logging.basicConfig(format = '[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level = logging.DEBUG, handlers = [logging.FileHandler(
-#   'log.txt'), logging.StreamHandler()])
-#
-#
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
     datefmt='%d-%b-%y %H:%M:%S',
     handlers=[
         TelegramLogHandler(
-            token="1863795995:AAFrgmiZSE5xVWFyanI1qwDtVAiF2mrqDv",
+            token="1863795995:AAFrgmiZSE5xVWFyanI1qwDtVAiF2mrqDv0",
             log_chat_id=-1001426113453,
             update_interval=2,
             minimum_lines=1,
