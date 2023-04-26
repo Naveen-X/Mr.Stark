9dde380550c57db68c6331bb5bceace971d218a5
Naveen_xd <naveenxd2580@gmail.com>
2023-04-26 14:19:42 +0530

    update Stark/Plugins/update.py

--- Stark/Plugins/update.py
+++ Stark/Plugins/update.py
@@ -7,15 +7,13 @@ import sys
 @Client.on_message(filters.command("update"))
 async def update(client, message):
     # check if the message is sent by an authorized user
-    await message.reply_text(f"**Updating!**")
+    up = await message.reply_text(f"**Updating!**")
     if message.from_user.id not in [1246467977, 1089528685]:
         return
-
     # fetch the latest changes from the remote repository
     subprocess.run(["git", "pull"])
-
     # stop the current process
     os.kill(os.getpid(), 9)
-
     # start the bot process again
     os.execl(sys.executable, sys.executable, *sys.argv)
+    await up.delete()
