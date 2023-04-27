20482fd310104ee68cc050b9a4d6c6263397eb89
Naveen_xd <naveenxd2580@gmail.com>
2023-04-27 08:17:32 +0530

    update Stark/__init__.py

--- Stark/__init__.py
+++ Stark/__init__.py
@@ -1,6 +1,30 @@
+import os
+import sys
+import time
 import logging
+import pyrogram
+import traceback 
+
+from pyrogram import idle
+from pyrogram.types import Message
+from traceback import format_exc
+
 from tglogging import TelegramLogHandler
 
+def error_handling(func):
+    async def inner(bot,message): #kwargs mean any other args avai
+        try:
+            if func.__name__ != 'on_message':
+                await bot.send_message(-1001426113453,'👤 /'+func.__name__)
+                pass
+            await func(bot,messag)
+        except BaseException as error:
+            fullerror = "".join(traceback.TracebackException.from_exception(error).format())
+            printerror = await bot.send_message(-1001426113453,f'❌ **{error}**\n```\n{fullerror}\n```\n\n__{str(format_exc)}__', disable_web_page_preview=True)
+            await bot.send_message(message.chat.id,f"❌ **An unexpected error has occur** \n```\n{error}\n```\nWe are sorry for that. [Fullerror]({printerror.link})")
+            traceback.print_exception(type(error), error, error.__traceback__, file = sys.stderr)
+    return inner  
+    
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
