ceec74f03d5e1ff0aae633d2d0e5e55fe8b82c36
Naveen_xd <naveenxd2580@gmail.com>
2023-05-01 08:55:43 +0530

    update main/helper_func/inline_funcs.py

---
+++ main/helper_func/inline_funcs.py
@@ -0,0 +1,115 @@
+import os
+import sys
+import asyncio
+from html import escape
+from re import sub as re_sub
+from time import ctime, ctime
+from contextlib import suppress
+from sys import version as pyver
+
+from pyrogram import enums, filters
+from pyrogram.raw.functions import Ping
+from pyrogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineQueryResultArticle,
+    InlineQueryResultPhoto,
+    InputTextMessageContent,
+    InlineQueryResultCachedDocument,
+)
+
+ARQ_URI = "https://arq.hamker.in"
+API_KEY = "IDIHNB-KATKEW-BGPKTB-ZTUHBX-ARQ"
+aiohttpsession = aiohttp.ClientSession()
+arq = ARQ(ARQ_URI, API_KEY, aiohttpsession)
+
+async def wall_func(answers, text):
+    results = await arq.wall(text)
+    if not results.ok:
+        answers.append(
+            InlineQueryResultArticle(
+                title="Error",
+                description=results.result,
+                input_message_content=InputTextMessageContent(results.result),
+            )
+        )
+        return answers
+    results = results.result[0:48]
+    for i in results:
+        answers.append(
+            InlineQueryResultPhoto(
+                photo_url=i.url_image,
+                thumb_url=i.url_thumb,
+                caption=f"[Source]({i.url_image})",
+            )
+        )
+    return answers
+
+async def torrent_func(answers, text):
+    results = await arq.torrent(text)
+    if not results.ok:
+        answers.append(
+            InlineQueryResultArticle(
+                title="Error",
+                description=results.result,
+                input_message_content=InputTextMessageContent(results.result),
+            )
+        )
+        return answers
+    results = results.result[0:48]
+    for i in results:
+        title = i.name
+        size = i.size
+        seeds = i.seeds
+        leechs = i.leechs
+        upload_date = i.uploaded
+        magnet = i.magnet
+        caption = f"""
+**Title:** __{title}__
+**Size:** __{size}__
+**Seeds:** __{seeds}__
+**Leechs:** __{leechs}__
+**Uploaded:** __{upload_date}__
+**Magnet:** `{magnet}`"""
+
+        description = f"{size} | {upload_date} | Seeds: {seeds}"
+        answers.append(
+            InlineQueryResultArticle(
+                title=title,
+                description=description,
+                input_message_content=InputTextMessageContent(
+                    caption, disable_web_page_preview=True
+                ),
+            )
+        )
+    return answers
+
+async def image_func(answers, query):
+    results = await arq.image(query)
+    if not results.ok:
+        answers.append(
+            InlineQueryResultArticle(
+                title="Error",
+                description=results.result,
+                input_message_content=InputTextMessageContent(results.result),
+            )
+        )
+        return answers
+    results = results.result[:49]
+    buttons = InlineKeyboard(row_width=2)
+    buttons.add(
+        InlineKeyboardButton(
+            text="Search again",
+            switch_inline_query_current_chat="image",
+        ),
+    )
+    for i in results:
+        answers.append(
+            InlineQueryResultPhoto(
+                title=i.title,
+                photo_url=i.url,
+                thumb_url=i.url,
+                reply_markup=buttons,
+            )
+        )
+    return answers
