3fd4fdc68e83dec394dceb974d950b0015cdb289
Naveen_xd <naveenxd2580@gmail.com>
2023-05-03 20:39:03 +0530

    update Stark/Plugins/telegraph.py

---
+++ Stark/Plugins/telegraph.py
@@ -0,0 +1,47 @@
+import os 
+from telegraph import Telegraph, exceptions, upload_file
+
+from Stark import error_handler
+from pyrogram import Client, filters
+from main.helper_func.plugin_helpers import convert_to_image
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+telegraph = Telegraph()
+r = telegraph.create_account(short_name="Mr.Stark")
+auth_url = r["auth_url"]
+
+@Client.on_message(filters.command(["telegraph", "tgraph"]))
+@error_handler
+async def telegrapher(c, m):
+  tg = await m.reply_text("`Please wi8..`")
+  if not m.reply_to_message:
+    await tg.edit("`This command needs reply to work..`")
+    return
+  if m.reply_to_message.media:
+    if m.reply_to_message.sticker:
+      m_d = await convert_to_image(m, c)
+    else:
+      m_d = await m.reply_to_message.download()
+    try:
+      media_url = upload_file(m_d)
+    except exceptions.TelegraphException as exc:
+      await tg.edit("Telegraph failed\n`{exc}`")
+      os.remove(m_d)
+      return
+  elif m.reply_to_message.text:
+    page_title = m.text.split(None, 1)[1] or m.from_user.first_name
+    page_text = m.reply_to_message.text
+    page_text = page_text.replace("\n", "</br>")
+    response = telegraph.create_page(page_title, html_content=page_text)
+    except exceptions.TelegraphException as exc:
+      await tg.edit("Telegram Failed\n `{exc}`")
+      return
+    wow_graph = "__Uploaded to Telegraph__"
+    url = "https://telegra.ph/" + respomse['path']
+    await tg.edit(
+      wow_graph, reply_markup=InlineKeyboardMarkup(
+        [
+          InlineKeyboardButton(wow_graph, url)
+          ]
+        )
+      )
