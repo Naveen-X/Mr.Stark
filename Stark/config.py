da17f1321ef387c608257aa6f619b13a504ca547
Naveen_xd <naveenxd2580@gmail.com>
2023-05-06 22:22:16 +0530

    update Stark/config.py

--- Stark/config.py
+++ Stark/config.py
@@ -10,5 +10,3 @@ class Config(object):
     API_ID = int(os.environ.get("API_ID", 1))
     API_HASH = os.environ.get("API_HASH", None)
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-    PRO_USERS = set(int(x) for x in os.environ.get("PRO_USERS", "").split())
-    PRO = list(PRO_USERS)
