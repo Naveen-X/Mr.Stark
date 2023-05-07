f8485f2ebf0c6685de903e48df1d338a57f1b70d
Naveen_xd <naveenxd2580@gmail.com>
2023-05-07 10:04:51 +0530

    update Stark/config.py

--- Stark/config.py
+++ Stark/config.py
@@ -10,3 +10,7 @@ class Config(object):
     API_ID = int(os.environ.get("API_ID", 1))
     API_HASH = os.environ.get("API_HASH", None)
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+    IG_SESSION = os.environ.get("IG_SESSION", None)
+    SPT_CLIENT = os.environ.get("SPT_CLIENT", None)
+    SPT_SECRET = os.environ.get("SPT_SECRET", None)
+    MONGO_DB = os.environ.get("MONGO_DB", None)
