f04d783c972958e96d445aaa793bfee7d69998d6
Naveen_xd <naveenxd2580@gmail.com>
2023-05-07 09:51:15 +0530

    update Stark/Plugins/spotify.py

--- Stark/Plugins/spotify.py
+++ Stark/Plugins/spotify.py
@@ -3,7 +3,8 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 from Stark import error_handler
-from pyrogram import Client, filters, enums
+from pyrogram import Client, filters
+from pyrogram.enums import ParseMode
 
 client_credentials_manager = SpotifyClientCredentials(client_id='e5f24fc4807b4bb6afa62e92fb607875', client_secret='08a5ff44ba7d47e4867983392b5a7ef0')
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
@@ -24,4 +25,4 @@ async def spotify_search(c, m):
       name = f"{track['name']} - {track['artists'][0]['name']}"
       msg = f"‎‎  ➣ **[{name} 🎧]({lnk})**\n"
       m = m+msg 
-  await spt.edit(m, disable_web_page_preview=True, parse_mode = "md")
+  await spt.edit(m, disable_web_page_preview=True, parse_mode = ParseMode.MARKDOWN))
