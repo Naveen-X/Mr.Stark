6561724a837696ee16c0a1d7b5ade8b9b176c68d
Satyendra <satya@devh.in>
2023-05-07 11:55:22 +0530

    Help

--- Stark/Plugins/help.py
+++ Stark/Plugins/help.py
@@ -8,9 +8,9 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyromod.helpers import ikb
 from pyromod.nav import Pagination
 
+from Stark import db
+from Stark.Plugins.start import keyboard
 from help_mod import Script
-from Script import script
-from Stark import error_handler, db
 from main.helper_func.basic_helpers import get_readable_time
 
 bot_start_time = time.time()
@@ -51,66 +51,117 @@ def item_title(item, page):
     return f'{item}'
 
 
-@Client.on_message(filters.command(['help', 'hlp', 'h']))
-@error_handler
-async def hi(c, m):
-    objects = [x for x in dir(Script) if not x.startswith('__')]
-    page = Pagination(
-        objects,
-        page_data=page_data,
-        item_data=item_data,
-        item_title=item_title
-    )
-    index = 0
-    lines = 4
-    columns = 3
-    kb = page.create(index, lines, columns)
-    await m.reply('Help Menu of Stark!', reply_markup=ikb(kb))
+@Client.on_message(filters.command(['help', 'h', 'hlp']))
+async def start(client, message):
+    id = message.from_user.id
 
-
-keyboard = ikb([
-    [("😎 About me 😎", 'about'), ('🖥System stats 🖥', 'sys_info')],
-    [('🤡Commands Help🤡', 'hlp')]
-])
-
-
-@Client.on_callback_query()
-async def cbdta(client, query):
-    q = query
-    data = query.data
-    # print(q.data)
-    if "close" in q.data:
-        await q.answer('Wait, Why?\nDelete if you can! i cant.')
-    elif "help_" in q.data:
-        hlp = q.data.split('help_')[1]
-        # print(hlp)
-        await q.edit_message_text(text=(getattr(Script, hlp)), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(
-            text='Back', callback_data='hlp'), InlineKeyboardButton(text='Home', callback_data='back')], [
-            InlineKeyboardButton(
-                text='System Stats',
-                callback_data='sys_info'),
-            InlineKeyboardButton(
-                text='About me',
-                callback_data='about')],
-            [InlineKeyboardButton(
-                text='Close',
-                callback_data='close'), ]]))
-    elif "hlp" in q.data:
-        objects = [x for x in dir(Script) if not x.startswith('__')]
+    def get_help_menu(page_):
+        objects = [x.replace('_', " ") for x in dir(Script) if not x.startswith('__')]
         page = Pagination(
             objects,
             page_data=page_data,
             item_data=item_data,
             item_title=item_title
         )
-        index = 0
-        lines = 4
+        index = page_
+        lines = 3
         columns = 3
-        kb = page.create(index, lines, columns)
-        await q.message.edit('Help Menu of Stark!', reply_markup=ikb(kb))
-    elif data == "about":
+        kb = page.create(page=index, lines=lines, columns=columns)
+        return kb
+
+    # Define the functions that generate data for the pagination
+    def page_data(page):
+        return f'{id}.page_{page}'
+
+    def item_data(item, page):
+        return f'{id}.help_{item}'
+
+    def item_title(item, page):
+        return f'{item}'
+
+    await message.reply_text('Help Menu of Stark!', reply_markup=ikb(get_help_menu(1)))
+
+
+@Client.on_callback_query()
+async def cb_handler(client, query):
+    sent_by = query.data.split('.')[0]
+    clicked_by = query.from_user.id
+    if int(sent_by) != int(clicked_by):
+        await query.answer('This is not for you!', show_alert=True)
+        return
+
+    def page_data(page):
+        return f'{sent_by}.page_{page}'
+
+    def item_data(item, page):
+        return f'{sent_by}.help_{item}'
+
+    def item_title(item, page):
+        return f'{item}'
+
+    def get_help_menu(page_):
+        objects = [x.replace('_', " ") for x in dir(Script) if not x.startswith('__')]
+        page = Pagination(
+            objects,
+            page_data=page_data,
+            item_data=item_data,
+            item_title=item_title
+        )
+        index = page_
+        lines = 3
+        columns = 3
+        kb = page.create(page=index, lines=lines, columns=columns)
+        return kb
+
+    if 'help_' in query.data:
+        hlp = query.data.split('help_')[1]
+        # print(hlp)
+        msg = ""
+        text_ = list(getattr(Script, hlp))
+        for text__ in text_:
+            desc = text__['desc']
+            cmds = list(text__['cmds'])
+            cmds = ', '.join(cmds)
+            usage = text__['usage']
+            msg = msg + """
+            
+<b>Info:</b> `{}`
+<b>Commands:</b> `{}`
+<b>Usage:</b> `{}`
+___
+            """.format(desc, cmds, usage)
+
+        await query.edit_message_text(text=msg, reply_markup=
+
+        ikb([
+            [('Back', f'{sent_by}.hlp'), ('Home', f'{sent_by}.back')],
+            [('System Stats', f'{sent_by}.sys_info'), ('About me', f'{sent_by}.about')],
+            [('Close', f'{sent_by}.close')]
+        ]))
+    # check if query.data is single digit integer
+    elif 'page_' in query.data:
+        # Define the functions that generate data for the pagination
+        page = int(query.data.split('page_')[1])
+        print(page)
+        try:
+            await query.edit_message_reply_markup(ikb(get_help_menu(page)))
+        except Exception as e:
+            await query.answer('Dumbo! you are at the same no. of the list', show_alert=True)
+    elif 'close' in query.data:
+        await query.message.delete()
+    elif "hlp" in query.data:
+        await query.message.edit_text('Help Menu of Stark!', reply_markup=ikb(get_help_menu(0)))
+    elif "about" in query.data:
         await query.message.edit_text(
-            text=f"<b>My name : <b/>Mr.Stark</i>\n<b>○ Creator : <a href='tg://user?id=1246467977'>Naveen_xD</a>\n○ Contributors:  <a href='tg://user?id=1089528685'>Satya</a>\n<a href='tg://user?id=1602293216'>Ashit</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram {__version__}</a></b>",
+            text=f"""
+<b>My name : <b/><i>Mr.Stark</i>
+<b>○ Made By :  </b> 
+    <a href='tg://user?id=1246467977'>Naveen_xD</a>
+    <a href='tg://user?id=1089528685'>Satya</a>
+    <a href='tg://user?id=1602293216'>Ashit</a>
+<b>○ Language : </b><code>Python3</code>
+<b>○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram {__version__}</a></b>
+""",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
@@ -120,12 +171,13 @@ async def cbdta(client, query):
                 ]
             )
         )
-    elif data == "back":
+    elif "back" in query.data:
         firstname = query.from_user.first_name
         await query.message.edit_text(
             text=f"<i>Hello, {firstname} !\nNice To Meet You, Well I Am A Powerfull Assistant bot For My Master!`\nMade by </i>Naveen_xD",
             reply_markup=keyboard,
+            disable_web_page_preview=True
         )
-    elif data == "sys_info":
+    elif 'sys_info' in query.data:
         text = await bot_sys_stats()
         await query.answer(text, show_alert=True)
--- help_mod.py
+++ help_mod.py
@@ -1,184 +1,182 @@
 class Script(object):
-  AI = [
-  {
-    "desc": "Generates AI response from OpenAI",
-    "cmds": ["gpt", "askgpt", "chatgpt"],
-    "usage": "/gpt Who are you?"
-  },
-  {
-    "desc": "Generates images using OpenAI",
-    "cmds": ["generate", "genimage"],
-    "usage": "/genimage an alien"
-  }
-]
-  CARBON = [
-  {
-    "desc": "Creates a carbon in doc format",
-    "cmds": ["carbon"],
-    "usage": "/carbon reply to a text message or give some text as input"
-  },
-  {
-    "desc": "Creates a carbon in image format",
-    "cmds": ["icarbon"],
-    "usage": "/carbon reply to a text message or give some text as input"
-  }
-]
-  DEV = [
-  {
-    "desc": "Executes python code",
-    "cmds": ["eval", "e"],
-    "usage": "/e <python code>"
-  },
-  {
-    "desc": "Run bash/terminal cmd",
-    "cmds": ["bash", "sh"],
-    "usage": "/bash <cmd>"
-  }
-]
-  FUN = [
-  {
-    "desc": "Get a cat image",
-    "cmds": ["cat"],
-  },
-  {
-    "desc": "Get a dog image",
-    "cmds": ["dog"],
-  },
-  {
-    "desc": "Get a random meme",
-    "cmds": ["meme"],
-  },
-  {
-    "desc": "Get a panda image",
-    "cmds": ["panda"],
-  }
-]
-  GOOGLE = [
-  {
-    "desc": "Google searcher!",
-    "cmds": ["gs", "google"],
-    "usage": "/gs <text to search>"
-  },
-]
-  INSTA_DL= [
-  {
-    "desc": "Download post/reel from instagram",
-    "cmds": ["insta", "instadl", "insdl", "instadownload"],
-    "usage": "/instadl <instagram post/reel link>"
-  }
-]
-  LOGOS = [
-  {
-    "desc": "Makes a logo for you with black bg",
-    "cmds": ["alogo"],
-    "usage": "/alogo <text for logo>"
-  },
-  {
-    "desc": "Makes a logo for you, try it out",
-    "cmds": ["slogo"],
-    "usage": "/slogo <text for logo>"
-  }
-]
-  PORT = [
-  {
-    "desc": "Scans open ports of an ip address",
-    "cmds": ["scanip"],
-    "usage": "/scanip <domain/ip>"
-  }
-]
-  PASTE = [
-  {
-    "desc": "Pastes the given text in spacebin",
-    "cmds": ["paste"],
-    "usage": "/paste <reply to message/text file>"
-  }
-]
-  QR = [
-  {
-    "desc": "Generates qr for given text",
-    "cmds": ["qr"],
-    "usage": "/qr <text to make qr>"
-  }
-]
-  QUOTLY = [
-  {
-    "desc": "Converts your text into a quote",
-    "cmds": ["quote", "qt", "qu", "q"],
-    "usage": "/q <reply to a text message>"
-  }
-]
-  STICKER = [
-  {
-    "desc": "Creates a sticker with given text",
-    "cmds": ["stcr"],
-    "usage": "/stcr Mr.Stark"
-  }
-]
-  SYSTEM = [
-  {
-    "desc": "Ping-Pong",
-    "cmds": ["p", "ping"],
-  },
-  {
-    "desc": " whether the bot is alive or not",
-    "cmds": ["alive"],
-  },
-  {
-    "desc": "Restarts the bot",
-    "cmds": ["restart"],
-  }
-]
-  TELEGRAPH = [
-  {
-    "desc": "Creates a telegraph",
-    "cmds": ["/telegraph", "/tgraph"],
-    "usage": "/tgraph <title for the telegraph | reply to a text message>"
-  }
-]
-  TRANSLATE = [
-  {
-    "desc": "Translates the replied message",
-    "cmds": ["tr", "translate"],
-    "usage": "/tr <language code | reply to a text message>"
-  }
-]
-  UPDATE = [
-  {
-    "desc": "Updates the system",
-    "cmds": ["up", "update"],
-  },
-  {
-    "desc": "Deletes snippets in gitlab",
-    "cmds": ["d"],
-  }
-]
-  URL_UPLOADER = [
-  {
-    "desc": "download's file from given link",
-    "cmds": ["urlupload"],
-    "usage": "/urlupload [direct link of the file]"
-  }
-]
-  WASTED = [
-  {
-    "desc": "Gta-V wasted effect on replied image",
-    "cmds": ["wasted"],
-    "usage": "/wasted <reply to a photo>"
-  }
-]
-  WHOIS = [
-  {
-    "desc": "Know who the replied person is",
-    "cmds": ["info", "whois"],
-    "usage": "/info <user id/username | reply to a user message"
-  }
-]
-  WRITE = [
-  {
-    "desc": "Writes given text on a white paper",
-    "cmds": ["write"],
-    "usage": "/write hello"
-  }
-]
-
-
+    AI = [
+        {
+            "desc": "Generates AI response from OpenAI",
+            "cmds": ["gpt", "askgpt", "chatgpt"],
+            "usage": "/gpt Who are you?"
+        },
+        {
+            "desc": "Generates images using OpenAI",
+            "cmds": ["generate", "genimage"],
+            "usage": "/genimage an alien"
+        }
+    ]
+    CARBON = [
+        {
+            "desc": "Creates a carbon in doc format",
+            "cmds": ["carbon"],
+            "usage": "/carbon reply to a text message or give some text as input"
+        },
+        {
+            "desc": "Creates a carbon in image format",
+            "cmds": ["icarbon"],
+            "usage": "/carbon reply to a text message or give some text as input"
+        }
+    ]
+    DEV = [
+        {
+            "desc": "Executes python code",
+            "cmds": ["eval", "e"],
+            "usage": "/e <python code>"
+        },
+        {
+            "desc": "Run bash/terminal cmd",
+            "cmds": ["bash", "sh"],
+            "usage": "/bash <cmd>"
+        }
+    ]
+    FUN = [
+        {
+            "desc": "Get a cat image",
+            "cmds": ["cat"],
+        },
+        {
+            "desc": "Get a dog image",
+            "cmds": ["dog"],
+        },
+        {
+            "desc": "Get a random meme",
+            "cmds": ["meme"],
+        },
+        {
+            "desc": "Get a panda image",
+            "cmds": ["panda"],
+        }
+    ]
+    GOOGLE = [
+        {
+            "desc": "Google searcher!",
+            "cmds": ["gs", "google"],
+            "usage": "/gs <text to search>"
+        },
+    ]
+    INSTA_DL = [
+        {
+            "desc": "Download post/reel from instagram",
+            "cmds": ["insta", "instadl", "insdl", "instadownload"],
+            "usage": "/instadl <instagram post/reel link>"
+        }
+    ]
+    LOGOS = [
+        {
+            "desc": "Makes a logo for you with black bg",
+            "cmds": ["alogo"],
+            "usage": "/alogo <text for logo>"
+        },
+        {
+            "desc": "Makes a logo for you, try it out",
+            "cmds": ["slogo"],
+            "usage": "/slogo <text for logo>"
+        }
+    ]
+    PORT = [
+        {
+            "desc": "Scans open ports of an ip address",
+            "cmds": ["scanip"],
+            "usage": "/scanip <domain/ip>"
+        }
+    ]
+    PASTE = [
+        {
+            "desc": "Pastes the given text in spacebin",
+            "cmds": ["paste"],
+            "usage": "/paste <reply to message/text file>"
+        }
+    ]
+    QR = [
+        {
+            "desc": "Generates qr for given text",
+            "cmds": ["qr"],
+            "usage": "/qr <text to make qr>"
+        }
+    ]
+    QUOTLY = [
+        {
+            "desc": "Converts your text into a quote",
+            "cmds": ["quote", "qt", "qu", "q"],
+            "usage": "/q <reply to a text message>"
+        }
+    ]
+    STICKER = [
+        {
+            "desc": "Creates a sticker with given text",
+            "cmds": ["stcr"],
+            "usage": "/stcr Mr.Stark"
+        }
+    ]
+    SYSTEM = [
+        {
+            "desc": "Ping-Pong",
+            "cmds": ["p", "ping"],
+        },
+        {
+            "desc": " whether the bot is alive or not",
+            "cmds": ["alive"],
+        },
+        {
+            "desc": "Restarts the bot",
+            "cmds": ["restart"],
+        }
+    ]
+    TELEGRAPH = [
+        {
+            "desc": "Creates a telegraph",
+            "cmds": ["/telegraph", "/tgraph"],
+            "usage": "/tgraph <title for the telegraph | reply to a text message>"
+        }
+    ]
+    TRANSLATE = [
+        {
+            "desc": "Translates the replied message",
+            "cmds": ["tr", "translate"],
+            "usage": "/tr <language code | reply to a text message>"
+        }
+    ]
+    UPDATE = [
+        {
+            "desc": "Updates the system",
+            "cmds": ["up", "update"],
+        },
+        {
+            "desc": "Deletes snippets in gitlab",
+            "cmds": ["d"],
+        }
+    ]
+    URL_UPLOADER = [
+        {
+            "desc": "download's file from given link",
+            "cmds": ["urlupload"],
+            "usage": "/urlupload [direct link of the file]"
+        }
+    ]
+    WASTED = [
+        {
+            "desc": "Gta-V wasted effect on replied image",
+            "cmds": ["wasted"],
+            "usage": "/wasted <reply to a photo>"
+        }
+    ]
+    WHOIS = [
+        {
+            "desc": "Know who the replied person is",
+            "cmds": ["info", "whois"],
+            "usage": "/info <user id/username | reply to a user message"
+        }
+    ]
+    WRITE = [
+        {
+            "desc": "Writes given text on a white paper",
+            "cmds": ["write"],
+            "usage": "/write hello"
+        }
+    ]
