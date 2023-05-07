e14b448a285d74eada33b7879e99e9f3907d8431
Naveen_xd <naveenxd2580@gmail.com>
2023-05-07 09:49:26 +0530

    update Stark/Plugins/spotify.py

--- Stark/Plugins/spotify.py
+++ Stark/Plugins/spotify.py
@@ -24,4 +24,4 @@ async def spotify_search(c, m):
       name = f"{track['name']} - {track['artists'][0]['name']}"
       msg = f"‎‎  ➣ **[{name} 🎧]({lnk})**\n"
       m = m+msg 
-  await spt.edit(m, disable_web_page_preview=True, parse_mode = "markdown")
+  await spt.edit(m, disable_web_page_preview=True, parse_mode = "md")
