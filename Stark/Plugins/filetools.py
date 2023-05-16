2b852aa7806e16900ce85dcf6ae9283063c8dd4a
Naveen_xD <naveenxd@devh.in>
2023-05-16 18:59:44 +0530

    update Stark/Plugins/filetools.py

--- Stark/Plugins/filetools.py
+++ Stark/Plugins/filetools.py
@@ -17,7 +17,6 @@ async def download(bot, message):
     if not message.reply_to_message.media:
         await dl.edit("`Reply to a message to download!`")
         return
-    c_time = time.time()
     Escobar = await message.reply_to_message.download(
         progress=progress, progress_args=("`Downloading This File!`", dl, s_time)
     )
@@ -26,3 +25,22 @@ async def download(bot, message):
     file_txt = "__Downloaded This File To__ `{}` __in__ `{}`."
 
     await dl.edit(file_txt.format(Escobar, dl_time))
+
+@Client.on_message(filters.command(["upload"]))
+@error_handler
+async def upload_file(c, m):
+    try:
+        file = m.text.split(None, 1)[1]
+    except IndexError:
+        await m.reply_text("What should I upload??")
+    if m.from_user.id not in [1246467977, 1089528685]:
+        if not file.startswith('downloads/'):
+            await m.reply_text("You are unauthorized..")
+        else:
+            msg = await m.reply_text("Uploading file please wait...")
+            await m.reply_document(file)
+            await msg.delete()
+    else:
+        msg = await m.reply_text("Uploading file please wait...")
+        await m.reply_document(file)
+        await msg.delete()
