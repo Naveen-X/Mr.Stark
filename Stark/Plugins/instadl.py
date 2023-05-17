11adb4032acf7c4d6064b21e26464f9576f41bb8
Naveen_xD <naveenxd@devh.in>
2023-05-17 12:51:27 +0530

    update Stark/Plugins/instadl.py

--- Stark/Plugins/instadl.py
+++ Stark/Plugins/instadl.py
@@ -162,3 +162,4 @@ async def instadl(c, m):
           await c.send_document(
             m.chat.id, f, caption=caption, force_document=False, reply_to_message_id=m.id, reply_markup=keyboard
          )
+        await msg.delete()
