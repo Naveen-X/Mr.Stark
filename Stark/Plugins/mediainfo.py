36208fd8ee522ce646d7ca79c53eed731e6969c5
Naveen_xD <naveenxd@devh.in>
2023-05-26 21:59:33 +0530

    update Stark/Plugins/mediainfo.py

--- Stark/Plugins/mediainfo.py
+++ Stark/Plugins/mediainfo.py
@@ -5,41 +5,12 @@ from Stark import error_handler
 from pyrogram.types import Message
 from pyrogram import Client, filters
 from main.helper_func.basic_helpers import runcmd
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 telegraph = Telegraph()
 tgnoob = telegraph.create_account(short_name="Stark")
 
 @Client.on_message(filters.command(['mediainfo', 'mediadata']))
-@error_handler
-async def media_info(c, m):
-    mi = await m.reply_text("`Processing..`")
-    if m.reply_to_message:
-      if m.reply_to_message.video:
-        await mi.edit("`Downloading media to get info\n Pls wi8..`")
-        file_path = await m.reply_to_message.download()
-        out, err, ret, pid = await runcmd(f"mediainfo '{file_path}'")
-        if not out:
-            await mi.edit("`Wtf, I Can't Determine This File Info`")
-            return
-        media_info = f"""
-    <code>           
-    {out}                  
-    </code>"""
-        title_of_page = "Media Info 🎬"
-        ws = media_info.replace("\n", "<br>")
-        response = telegraph.create_page(title_of_page, html_content=ws)
-        km = response["path"]
-        await mi.edit(f"`This MediaInfo Can Be Found` [Here](https://telegra.ph/{km})")
-        if os.path.exists(file_path):
-            os.remove(file_path)
-      else:
-        await mi.edit("`Reply to a video`")
-        return
-    else:
-        await mi.edit("`Reply to a video`")
-        return
-
-@Client.on_message(filters.command(['tinfo']))
 @error_handler
 async def media_info(_, message: Message):
     mi = await message.reply_text("Processing...")
@@ -55,7 +26,17 @@ async def media_info(_, message: Message):
         ws = media_info.replace("\n", "<br>")
         response = telegraph.create_page(title_of_page, html_content=ws)
         km = response["path"]
-        await mi.edit(f"This MediaInfo can be found [Here](https://telegra.ph/{km})")
+        keyboard = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text="Mediainfo",
+                    url=f"https://telegra.ph/{km}",
+                )
+            ]
+        ]
+    )
+        await mi.edit("**MediaInfo Gathered**")
         if os.path.exists(file_path):
             os.remove(file_path)
     else:
