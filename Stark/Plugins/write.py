2d9def0271f8300394a8344cf6a14d21708ffb4f
Naveen_xD <naveenxd@devh.in>
2023-05-31 15:07:45 +0530

    update Stark/Plugins/write.py

--- Stark/Plugins/write.py
+++ Stark/Plugins/write.py
@@ -4,6 +4,20 @@ from urllib.parse import quote as qt
 
 from Stark import error_handler
 
+def text_set(text):
+    lines = []
+    if len(text) <= 55:
+        lines.append(text)
+    else:
+        all_lines = text.split("\n")
+        for line in all_lines:
+            if len(line) <= 55:
+                lines.append(line)
+            else:
+                k = int(len(line) / 55)
+                for z in range(1, k + 2):
+                    lines.append(line[((z - 1) * 55) : (z * 55)])
+    return lines[:25]
 
 @Client.on_message(filters.command(["write"]))
 @error_handler
@@ -18,7 +32,7 @@ async def write(bot, message):
         await op.edit("`What do you wanna write?`")
         return
     try:
-        value = qt(text)
+        value = qt(text_set(text))
         url = f"https://api.naveenxd.wip.la/write?text={value}"
         await message.reply_photo(url)
         await op.delete()
