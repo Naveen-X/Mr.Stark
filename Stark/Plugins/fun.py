6a05d5b96554ef11d27720270b6d7381547a9e83
Naveen_xD <naveenxd@devh.in>
2023-05-31 14:55:22 +0530

    update Stark/Plugins/fun.py

--- Stark/Plugins/fun.py
+++ Stark/Plugins/fun.py
@@ -7,7 +7,7 @@ from Stark import error_handler
 @Client.on_message(filters.command(["meme"]))
 @error_handler
 async def meme(bot, message):
-    hmm_s = "https://some-random-api.ml/meme"
+    hmm_s = "https://some-random-api.com/meme"
     r = requests.get(url=hmm_s).json()
     image_s = r["image"]
     await bot.send_photo(message.chat.id, image_s, reply_to_message_id=message.id)
@@ -16,7 +16,7 @@ async def meme(bot, message):
 @Client.on_message(filters.command(["panda"]))
 @error_handler
 async def panda(bot, message):
-    link = "https://some-random-api.ml/img/panda"
+    link = "https://some-random-api.com/img/panda"
     r = requests.get(url=link).json()
     image_s = r["link"]
     await bot.send_photo(message.chat.id, image_s, reply_to_message_id=message.id)
@@ -25,7 +25,7 @@ async def panda(bot, message):
 @Client.on_message(filters.command(["cat"]))
 @error_handler
 async def cat(bot, message):
-    link = "https://some-random-api.ml/img/cat"
+    link = "https://some-random-api.com/img/cat"
     r = requests.get(url=link).json()
     image_s = r["link"]
     await bot.send_photo(message.chat.id, image_s, reply_to_message_id=message.id)
@@ -34,7 +34,7 @@ async def cat(bot, message):
 @Client.on_message(filters.command(["dog"]))
 @error_handler
 async def dog(bot, message):
-    link = "https://some-random-api.ml/img/dog"
+    link = "https://some-random-api.com/img/dog"
     r = requests.get(url=link).json()
     image_s = r["link"]
     await bot.send_photo(message.chat.id, image_s, reply_to_message_id=message.id)
