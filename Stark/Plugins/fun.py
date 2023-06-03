ca901a7f3371281f9d719b76941741c18f70605b
Naveen_xD <naveenxd@devh.in>
2023-06-03 18:56:48 +0530

    Api has been removed so deleted meme cmd

--- Stark/Plugins/fun.py
+++ Stark/Plugins/fun.py
@@ -4,15 +4,6 @@ from pyrogram import Client, filters
 from Stark import error_handler
 
 
-@Client.on_message(filters.command(["meme"]))
-@error_handler
-async def meme(bot, message):
-    hmm_s = "https://some-random-api.com/meme"
-    r = requests.get(url=hmm_s).json()
-    image_s = r["image"]
-    await bot.send_photo(message.chat.id, image_s, reply_to_message_id=message.id)
-
-
 @Client.on_message(filters.command(["panda"]))
 @error_handler
 async def panda(bot, message):
