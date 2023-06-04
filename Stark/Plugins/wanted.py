769ead597bf7473517f9550297fde29484ed5fa1
Naveen_xD <naveenxd@devh.in>
2023-06-04 12:28:38 +0530

    update Stark/Plugins/wanted.py

--- Stark/Plugins/wanted.py
+++ Stark/Plugins/wanted.py
@@ -18,6 +18,18 @@ async def wanted(c,m):
     wanted_poster = WantedPoster(photo_url, last_name, m.from_user.first_name, bounty_amount)
     path = wanted_poster.generate()
     await c.send_photo(chat_id=m.chat.id, photo=path)
+    os.remove(path)
     return
   if (m.text == "/wanted" or "/bounty" and m.reply_to_message):
-    await bt.edit("fine")
+    async for photo in c.get_chat_photos(m.reply_to_message.from_user.id, limit=1):
+      photo_url = await c.download_media(photo.file_id)
+    bounty_amount = 3_000_000_000
+    try:
+      last_name = m.reply_to_message.from_user.last_name
+    except:
+      last_name=None
+    wanted_poster = WantedPoster(photo_url, last_name, m.reply_to_message.from_user.first_name, bounty_amount)
+    path = wanted_poster.generate()
+    await c.send_photo(chat_id=m.chat.id, photo=path)
+    os.remove(path)
+    return
