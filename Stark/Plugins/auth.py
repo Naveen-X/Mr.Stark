848fc4fc19be51654ad01db4daedbe441a4315c4
Naveen_xD <naveenxd@devh.in>
2023-06-06 10:13:00 +0530

    update Stark/Plugins/auth.py

--- Stark/Plugins/auth.py
+++ Stark/Plugins/auth.py
@@ -11,32 +11,32 @@ async def auth_user(user_id):
 async def dis_auth_user(user_id):
     DB.auth.delete_one({"user_id": user_id})
 
-## I know This is wrong but its OK!
 
+## I know This is wrong but it's OK!
 
 @Client.on_message(filters.command(["auth"]) & filters.user([1246467977, 1089528685]))
 @error_handler
 async def qt_add(c, m):
-	if m.reply_to_message:
-    x = await m.reply_text("__Authorising User__")
-    try:
-      await auth_user(m.reply_to_message.from_user.id)
-      await x.edit("__Authorused__")
-    except:
-      await x.edit("__Error Occured__")
-  else:
-    await m.reply_text("`Reply to a user to Authorise him`")
+    if m.reply_to_message:
+        x = await m.reply_text("Authorising User")
+        try:
+            await auth_user(m.reply_to_message.from_user.id)
+            await x.edit("Authorised")
+        except Exception as e:
+            await x.edit("Error Occurred: " + str(e))
+    else:
+        await m.reply_text("Reply to a user to Authorise him")
 
 
 @Client.on_message(filters.command(["disauth", "unauth"]) & filters.user([1246467977, 1089528685]))
 @error_handler
 async def qt_remove(c, m):
-  if m.reply_to_message:
-    x = await m.reply_text("__UnAuthorising User__")
-    try:
-      await dis_auth_user(m.reply_to_message.from_user.id)
-      await x.edit("__UnAuthorused__")
-    except:
-      await x.edit("__User is Not Authorised Yet__")
-  else:
-    await m.reply_text("`Reply to a user to UnAuthorise him`")
+    if m.reply_to_message:
+        x = await m.reply_text("UnAuthorising User")
+        try:
+            await dis_auth_user(m.reply_to_message.from_user.id)
+            await x.edit("UnAuthorised")
+        except:
+            await x.edit("User is Not Authorised Yet")
+    else:
+        await m.reply_text("Reply to a user to UnAuthorise him")
