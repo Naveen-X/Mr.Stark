179ccdd33d2011ac08d5775a0bc808fb85a984b1
Naveen_xD <naveenxd@devh.in>
2023-06-08 10:29:16 +0530

    update start_req.sh

--- start_req.sh
+++ start_req.sh
@@ -2,4 +2,5 @@ git clone https://github.com/Naveen-X/Mr.Stark
 cd "Mr.Stark" && git pull && pip install -r req.txt
 apt-get update && apt upgrade -y
 apt-get install mediainfo -y
+apt-get install libgl1-mesa-glx
 python3 -m "Stark"
