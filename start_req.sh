7359888467c0a03db5223a86cc3dc88bd231d33d
Naveen_xD <naveenxd@devh.in>
2023-06-08 21:02:50 +0530

    update start_req.sh

--- start_req.sh
+++ start_req.sh
@@ -3,4 +3,5 @@ cd "Mr.Stark" && git pull && pip install -r req.txt
 apt-get update && apt upgrade -y
 apt-get install mediainfo -y
 apt-get install libgl1-mesa-glx
+apt-get install -y python3-opencv
 python3 -m "Stark"
