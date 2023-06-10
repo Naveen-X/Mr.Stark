5f19782e584f1d40b3110b609016a40867ca0054
Naveen_xD <naveenxd@devh.in>
2023-06-10 13:25:22 +0530

    update Stark/Plugins/wasted.py

--- Stark/Plugins/wasted.py
+++ Stark/Plugins/wasted.py
@@ -23,3 +23,21 @@ async def wasted(bot, message):
     await message.reply_photo(url)
     await gta.delete()
     os.remove(pic)
+
+@Client.on_message(filters.command(["passed"]))
+@error_handler
+async def wasted(bot, message):
+    gta = await bot.send_message(message.chat.id, "`Processing...`")
+    if not message.reply_to_message:
+        await gta.edit("`Reply to a photo :(`")
+        return
+    ok = message.reply_to_message
+    pic = await bot.download_media(ok)
+    poto_url = upload_file(pic)
+    imglink = f"https://telegra.ph{poto_url[0]}"
+
+    url = f"https://some-random-api.com/canvas/overlay/passed?avatar={imglink}"
+
+    await message.reply_photo(url)
+    await gta.delete()
+    os.remove(pic)
