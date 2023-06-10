9dc0fd7ba06e9ed41b14b7c2644c99297c9c068d
Naveen_xD <naveenxd@devh.in>
2023-06-10 13:05:41 +0530

    update Stark/Plugins/fun.py

--- Stark/Plugins/fun.py
+++ Stark/Plugins/fun.py
@@ -36,3 +36,35 @@ async def bored(bot, message):
     api = requests.get("https://nekos.best/api/v2/bored").json()
     url = api["results"][0]['url']
     await message.reply_animation(url)
+
+@Client.on_message(filters.command(["pikachu"]))
+@error_handler
+async def pikachu(bot, message):
+    link = "https://some-random-api.com/img/pikachu"
+    r = requests.get(url=link).json()
+    url = r["link"]
+    await message.reply_animation(url)
+
+@Client.on_message(filters.command(["pat"]))
+@error_handler
+async def pat(bot, message):
+    link = "https://some-random-api.com/img/pat"
+    r = requests.get(url=link).json()
+    url = r["link"]
+    await message.reply_animation(url)
+
+@Client.on_message(filters.command(["wink"]))
+@error_handler
+async def wink(bot, message):
+    link = "https://some-random-api.com/img/wink"
+    r = requests.get(url=link).json()
+    url = r["link"]
+    await message.reply_animation(url)
+
+@Client.on_message(filters.command(["hug"]))
+@error_handler
+async def hug(bot, message):
+    link = "https://some-random-api.com/img/hug"
+    r = requests.get(url=link).json()
+    url = r["link"]
+    await message.reply_animation(url)
