b9b91aa8503e311eb7d98f8c72ee4551b1001c69
Naveen_xD <naveenxd@devh.in>
2023-06-13 14:16:10 +0530

    update start_req.sh

--- start_req.sh
+++ start_req.sh
@@ -2,7 +2,6 @@ git clone https://github.com/Naveen-X/Mr.Stark
 cd "Mr.Stark" && git pull && pip install -r req.txt
 apt-get update && apt upgrade -y
 apt-get install mediainfo -y
-apt-get install libgl1-mesa-glx
 apt-get install -y python3-opencv
 sudo apt-get install ffmpeg -y
 python3 -m "Stark"
