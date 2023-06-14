51e6cacd9afd7ea2c6355cc39fbef9041bae424a
Naveen_xD <naveenxd@devh.in>
2023-06-14 21:42:03 +0530

    update Stark/Plugins/quotly.py

--- Stark/Plugins/quotly.py
+++ Stark/Plugins/quotly.py
@@ -1,6 +1,4 @@
 import base64
-
-import requests
 from pyrogram import Client, filters
 
 from Stark import error_handler
@@ -12,139 +10,80 @@ async def quote(client, m):
     qse = await m.reply_text("`Quoting..`")
     u = m.from_user
     messages = []
-    uuy = {
-        "id": u.id,
-        "first_name": u.first_name,
-        "last_name": u.last_name,
-        "username": u.username,
-        "language_code": "eu",
-        "title": f"{m.from_user.first_name} {m.from_user.last_name}",
-        "photo": {
-            "small_file_id": u.photo.small_file_id,
-            "small_file_unique_id": u.photo.small_photo_unique_id,
-            "big_file_id": u.photo.big_file_id,
-            "big_file_unique_id": u.photo.big_photo_unique_id
-        },
-        "type": "private",
-        "name": f"{m.from_user.first_name} {m.from_user.last_name}"
-    }
 
-    if m.reply_to_message and len(m.command) > 1:
-        num = m.reply_to_message.id
-        if m.command[1].isdigit():
-            for i in range(int(m.command[1])):
-                mes = await client.get_messages(m.chat.id, num + i)
-                u = mes.from_user
-                if not u.first_name:
-                    first_name = ""
-                else:
-                    first_name = u.first_name
-                if not u.last_name:
-                    last_name = ""
-                else:
-                    last_name = u.last_name
-                if u.photo:
-                    small_id = "u.photo.small_file_id"
-                    small_unique = "u.photo.small_photo_unique_id"
-                    big_id = "u.photo.big_file_id"
-                    big_unique = "u.photo.big_photo_unique_id"
-                else:
-                    small_id, small_unique, big_id, big_unique = None
-                uu = {
-                    "id": u.id,
-                    "first_name": first_name,
-                    "last_name": last_name,
-                    "username": u.username,
-                    "language_code": "eu",
-                    "title": f"{first_name} {last_name}",
-                    "photo": {
-                        "small_file_id": small_id,
-                        "small_file_unique_id": small_unique,
-                        "big_file_id": big_id,
-                        "big_file_unique_id": big_unique
-                    },
-                    "type": "private",
-                    "name": f"{first_name} {last_name}"
-                }
-                me = {
-                    "entities": [],
-                    "chatId": m.chat.id,
-                    "avatar": True,
-                    "from": uu,
-                    "text": mes.text,
-                    "replyMessage": {}
-                }
-                messages.append(me)
-
-    if (m.text == "/q" or m.text=="/qu" or m.text=="/qt" or m.text=="/quote") and (m.reply_to_message):
-        mes = m.reply_to_message
-        u = mes.from_user
-        if not u.first_name:
+    def create_user_dict(user):
+        if not user.first_name:
             first_name = ""
         else:
-            first_name = u.first_name
-        if not u.last_name:
+            first_name = user.first_name
+        if not user.last_name:
             last_name = ""
         else:
-            last_name = u.last_name
+            last_name = user.last_name
+        if user.photo:
+            small_id = user.photo.small_file_id
+            small_unique = user.photo.small_photo_unique_id
+            big_id = user.photo.big_file_id
+            big_unique = user.photo.big_photo_unique_id
+        else:
+            small_id = small_unique = big_id = big_unique = None
+
+        return {
+            "id": user.id,
+            "first_name": first_name,
+            "last_name": last_name,
+            "username": user.username,
+            "language_code": "eu",
+            "title": f"{first_name} {last_name}",
+            "photo": {
+                "small_file_id": small_id,
+                "small_file_unique_id": small_unique,
+                "big_file_id": big_id,
+                "big_file_unique_id": big_unique
+            },
+            "type": "private",
+            "name": f"{first_name} {last_name}"
+        }
+
+    if m.text in ["/q", "/qu", "/qt", "/quote"] and m.reply_to_message:
+        mes = m.reply_to_message
+        u = mes.from_user
         if mes.reply_to_message:
-            uu = {
-                "id": u.id,
-                "first_name": first_name,
-                "last_name": last_name,
-                "username": u.username,
-                "language_code": "eu",
-                "title": f"{first_name} {last_name}",
-                "photo": {
-                    "small_file_id": u.photo.small_file_id,
-                    "small_file_unique_id": u.photo.small_photo_unique_id,
-                    "big_file_id": u.photo.big_file_id,
-                    "big_file_unique_id": u.photo.big_photo_unique_id
-                },
-                "type": "private",
-                "name": f"{first_name} {last_name}"
+            reply_msg = {
+                "text": mes.reply_to_message.text,
+                "name": mes.reply_to_message.from_user.first_name,
+                "chatId": mes.reply_to_message.from_user.id
             }
-            me = {
+        else:
+            reply_msg = {}
+
+        messages.append({
+            "entities": [],
+            "chatId": m.chat.id,
+            "avatar": True,
+            "from": create_user_dict(u),
+            "text": mes.text,
+            "replyMessage": reply_msg
+        })
+
+    elif m.reply_to_message and len(m.command) > 1 and m.command[1].isdigit():
+        num = m.reply_to_message.id
+        for i in range(int(m.command[1])):
+            mes = await client.get_messages(m.chat.id, num + i)
+            u = mes.from_user
+            messages.append({
                 "entities": [],
                 "chatId": m.chat.id,
                 "avatar": True,
-                "from": uu,
+                "from": create_user_dict(u),
                 "text": mes.text,
-                "replyMessage": {
-                    "text": mes.reply_to_message.text,
-                    "name": mes.reply_to_message.from_user.first_name,
-                    "chatId": mes.reply_to_message.from_user.id
-                }
-            }
-            messages.append(me)
-        if not mes.reply_to_message and mes:
-            uu = {
-                "id": u.id,
-                "first_name": first_name,
-                "last_name": last_name,
-                "username": u.username,
-                "language_code": "eu",
-                "title": f"{first_name} {last_name}",
-                "photo": {
-                    "small_file_id": u.photo.small_file_id,
-                    "small_file_unique_id": u.photo.small_photo_unique_id,
-                    "big_file_id": u.photo.big_file_id,
-                    "big_file_unique_id": u.photo.big_photo_unique_id
-                },
-                "type": "private",
-                "name": f"{first_name} {last_name}"
-            }
-            me = {
-                "entities": [],
-                "chatId": m.chat.id,
-                "avatar": True,
-                "from": uu,
-                "text": mes.text,
-            }
-            messages.append(me)
-    if (m.text == "/q" or m.text=="/qu" or m.text=="/qt" or m.text=="/quote") and (not m.reply_to_message):
-      await qse.edit("`Reply to a text message or guve text along with command`")
-      return
+                "replyMessage": {}
+            })
+
+    elif m.text in ["/q", "/qu", "/qt", "/quote"] and not m.reply_to_message:
+        await qse.edit("`Reply to a text message or give text along with the command`")
+        return
+
     text = {
         "type": "quote",
         "format": "png",
@@ -153,8 +92,8 @@ async def quote(client, m):
         "height": 768,
         "scale": 2,
         "messages": messages
-
     }
+
     r = requests.post("https://bot.lyo.su/quote/generate", json=text)
     image = r.json()["result"]["image"]
     im = base64.b64decode(image.encode('utf-8'))
