0f9b951b61e4cfc5d4b7ffad6e578b97162d7e87
Naveen_xD <naveenxd@devh.in>
2023-06-14 13:29:19 +0530

    update Stark/Plugins/pinterest.py

--- Stark/Plugins/pinterest.py
+++ Stark/Plugins/pinterest.py
@@ -1,5 +1,5 @@
 from requests import get
-from bs4 import BeautifulSoup as s
+from bs4 import BeautifulSoup as bs
 from pyrogram import Client, filters
 
 from Stark import error_handler
@@ -8,40 +8,38 @@ from Stark import error_handler
 @error_handler
 async def pinterest_dl(c, m):
     try:
-      x = m.text.split(None, 1)[1]
+        x = m.text.split(None, 1)[1]
     except IndexError:
-      await m.reply_text("`Please provide a Pinterest URL.`")
-      return
+        await m.reply_text("`Please provide a Pinterest URL.`")
+        return
+    
     url = x.strip()
-    if url.startswith("https://in.pinterest.com/pin/") or ("https://www.pinterest.com/pin/"):
+    if url.startswith(("https://in.pinterest.com/pin/", "https://www.pinterest.com/pin/")):
         try:
             response = get(url)
-            soup = s(response.text, "html.parser")
+            soup = bs(response.text, "html.parser")
             image_urls = [n.get('src') for n in soup.find_all("img")]
-            if len(image_urls) > 0:
+            if image_urls:
                 await c.send_photo(chat_id=m.chat.id, photo=image_urls[0])
             else:
                 await m.reply_text("`No images found on the given Pinterest URL.`")
         except Exception as e:
             await m.reply_text("`An error occurred while fetching and sending the image. Please try again later.`")
-            print(f"Error: `{e}`")
-            return
-    if url.startswith("https://pin.it/"):
+            print(f"Error: {e}")
+    elif url.startswith("https://pin.it/"):
         response = get(url)
         redirected_url = response.url
         t_url = redirected_url
         f_url = t_url.split("sent")[0]
         try:
             response = get(f_url)
-            soup = s(response.text, "html.parser")
+            soup = bs(response.text, "html.parser")
             image_urls = [n.get('src') for n in soup.find_all("img")]
-            if len(image_urls) > 0:
+            if image_urls:
                 await c.send_photo(chat_id=m.chat.id, photo=image_urls[0])
             else:
                 await m.reply_text("`No images found on the given Pinterest URL.`")
         except Exception as e:
-            await m.reply_text(f"`An error occurred while fetching and sending the image. Please try again later.`\n**Error:** `{e}`")
-            return
-    if not url.startswith ("https://pin.it/") or ("https://in.pinterest.com/pin/"):
-      await m.reply_text("`Invalid Pinterest URL. Please provide a valid URL.`")
-      return
+            await m.reply_text(f"`An error occurred while fetching and sending the image. Please try again later.`\n**Error:** {e}")
+    else:
+        await m.reply_text("`Invalid Pinterest URL. Please provide a valid URL.`")
