58e61aac6fae01b9bc13aa4935cf1fad5f721be5
Naveen_xD <naveenxd@devh.in>
2023-06-16 10:26:24 +0530

    update start_req.sh

--- start_req.sh
+++ start_req.sh
@@ -2,6 +2,7 @@ git clone https://github.com/Naveen-X/Mr.Stark
 cd "Mr.Stark" && git pull && pip install -r req.txt
 apt-get update && apt upgrade -y
 apt-get install mediainfo -y
+sudo apt-get install libgl1-mesa-glx
 apt-get install -y python3-opencv
 sudo apt-get install ffmpeg -y
 sudo apt-get install gifsicle
