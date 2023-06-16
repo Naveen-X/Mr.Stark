5bf0d5b9e31ac41edca24a2146dd32b61e9ac926
Naveen_xD <naveenxd@devh.in>
2023-06-16 10:31:19 +0530

    update main/helper_func/inline_funcs.py

--- main/helper_func/inline_funcs.py
+++ main/helper_func/inline_funcs.py
@@ -1,33 +1,30 @@
-import os
-import sys
 import aiohttp
 import asyncio
-from html import escape
-from Python_ARQ import ARQ
-from re import sub as re_sub
-from time import ctime, ctime
-from contextlib import suppress
-from sys import version as pyver
+import re
+import sys
 
-from pyrogram import enums, filters
+from pyrogram import filters
 from pyrogram.raw.functions import Ping
 from pyrogram.types import (
     CallbackQuery,
     InlineKeyboardButton,
     InlineQueryResultArticle,
     InlineQueryResultPhoto,
-    InputTextMessageContent,
     InlineQueryResultCachedDocument,
+    InputTextMessageContent,
+    InlineKeyboardMarkup,
 )
-from Stark.config import Config 
+from arq import ARQ
+from Stark.config import Config
 
 ARQ_URI = "https://arq.hamker.in"
 API_KEY = Config.ARQ_API
 aiohttpsession = aiohttp.ClientSession()
 arq = ARQ(ARQ_URI, API_KEY, aiohttpsession)
 
-async def wall_func(answers, text):
-    results = await arq.wall(text)
+
+async def wall_func(answers, query):
+    results = await arq.wall(query)
     if not results.ok:
         answers.append(
             InlineQueryResultArticle(
@@ -37,7 +34,7 @@ async def wall_func(answers, text):
             )
         )
         return answers
-    results = results.result[0:48]
+    results = results.result[:10]
     for i in results:
         answers.append(
             InlineQueryResultPhoto(
@@ -48,8 +45,9 @@ async def wall_func(answers, text):
         )
     return answers
 
-async def torrent_func(answers, text):
-    results = await arq.torrent(text)
+
+async def torrent_func(answers, query):
+    results = await arq.torrent(query)
     if not results.ok:
         answers.append(
             InlineQueryResultArticle(
@@ -59,7 +57,7 @@ async def torrent_func(answers, text):
             )
         )
         return answers
-    results = results.result[0:48]
+    results = results.result[:10]
     for i in results:
         title = i.name
         size = i.size
@@ -68,12 +66,12 @@ async def torrent_func(answers, text):
         upload_date = i.uploaded
         magnet = i.magnet
         caption = f"""
-**Title:** __{title}__
-**Size:** __{size}__
-**Seeds:** __{seeds}__
-**Leechs:** __{leechs}__
-**Uploaded:** __{upload_date}__
-**Magnet:** `{magnet}`"""
+        **Title:** __{title}__
+        **Size:** __{size}__
+        **Seeds:** __{seeds}__
+        **Leechs:** __{leechs}__
+        **Uploaded:** __{upload_date}__
+        **Magnet:** `{magnet}`"""
 
         description = f"{size} | {upload_date} | Seeds: {seeds}"
         answers.append(
@@ -87,6 +85,7 @@ async def torrent_func(answers, text):
         )
     return answers
 
+
 async def image_func(answers, query):
     results = await arq.image(query)
     if not results.ok:
@@ -98,8 +97,8 @@ async def image_func(answers, query):
             )
         )
         return answers
-    results = results.result[:49]
-    buttons = InlineKeyboard(row_width=2)
+    results = results.result[:10]
+    buttons = InlineKeyboardMarkup(row_width=2)
     buttons.add(
         InlineKeyboardButton(
             text="Search again",
