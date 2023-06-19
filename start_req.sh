aba9c7b8634e362811cfd0d13dd8a39dddf8c3f9
satya <dev@devh.in>
2023-06-19 14:01:53 +0530

    Idk whats wrong

--- start_req.sh
+++ start_req.sh
@@ -1,9 +1,70 @@
+#!/bin/bash
+#
+#RED='\033[0;31m'
+#GREEN='\033[0;32m'
+#YELLOW='\033[0;33m'
+#NC='\033[0m'
+#
+#echo_color() {
+#  local color=$1
+#  local message=$2
+#  echo -e "${color}${message}${NC}"
+#}
+#
+#echo_red() {
+#  local message=$1
+#  echo_color "${RED}" "$message"
+#}
+#
+#echo_green() {
+#  local message=$1
+#  echo_color "${GREEN}" "$message"
+#}
+#
+#echo_yellow() {
+#  local message=$1
+#  echo_color "${YELLOW}" "$message"
+#}
+
+#echo_red "This is an error message."
+#echo_green "This is a success message."
+#echo_yellow "This is a warning message."
+#echo "This is a normal message."
+echo -e "\e[32mCloning repository...\e[0m"
 git clone https://github.com/Naveen-X/Mr.Stark
+
+echo -e "\e[32mUpdating repository...\e[0m"
 cd "Mr.Stark" && git pull && pip install -r req.txt
-sudo apt-get update && apt upgrade -y
+
+echo -e "\e[32mUpdating system packages...\e[0m"
+sudo apt-get update && sudo apt-get upgrade -y
+
+echo -e "\e[32mInstalling mediainfo...\e[0m"
 sudo apt-get install mediainfo -y
+
+echo -e "\e[32mInstalling libgl1-mesa-glx...\e[0m"
 sudo apt-get install libgl1-mesa-glx -y
+
+echo -e "\e[32mInstalling python3-opencv...\e[0m"
 sudo apt-get install python3-opencv -y
+
+echo -e "\e[32mInstalling ffmpeg...\e[0m"
 sudo apt-get install ffmpeg -y
+
+echo -e "\e[32mInstalling gifsicle...\e[0m"
 sudo apt-get install gifsicle -y
+
+echo -e "\e[32mRunning Stark...\e[0m"
 python3 -m "Stark"
+
+#
+#
+#git clone https://github.com/Naveen-X/Mr.Stark
+#cd "Mr.Stark" && git pull && pip install -r req.txt
+#sudo apt-get update && apt upgrade -y
+#sudo apt-get install mediainfo -y
+#sudo apt-get install libgl1-mesa-glx -y
+#sudo apt-get install python3-opencv -y
+#sudo apt-get install ffmpeg -y
+#sudo apt-get install gifsicle -y
+#python3 -m "Stark"
