9f8ef456d636673afb6fb5cd1fa483d7c8cb4a8b
Naveen_xD <naveenxd@devh.in>
2023-06-21 10:16:39 +0530

    update Stark/Plugins/quotly.py

--- Stark/Plugins/quotly.py
+++ Stark/Plugins/quotly.py
@@ -156,7 +156,7 @@ async def quote(client, m):
         "backgroundColor": "#1b1429",
         "width": 512,
         "height": 768,
-        "scale": 4.5,
+        "scale": 4.2,
         "messages": messages
     }
 
