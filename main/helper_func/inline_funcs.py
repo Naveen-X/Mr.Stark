69ecdc988f4afbb63322ec0c8efae81542cd0c14
Naveen_xD <naveenxd@devh.in>
2023-06-22 19:58:39 +0530

    My Bad

--- main/helper_func/inline_funcs.py
+++ main/helper_func/inline_funcs.py
@@ -235,17 +235,17 @@ async def flipkart_search(answers, query):
             c_price = x.get("current_price")
             o_price = x.get("original_price")
             more_details = x.get("query_url")
-            link2 = requests.post("https://da.gd/s/", link).text
+            link2 = requests.post("https://da.gd/s/", more_details).text
             output = f"""
 **Title:** {name}
 **Price:** ~~{o_price}~~ ‎  __{c_price}__
 """
             keyboard = InlineKeyboardMarkup([
                 [
-                    InlineKeyboardButton(text="📱 View on Flipkart", url=link2)
+                    InlineKeyboardButton(text="📱 View on Flipkart", url=link)
                 ],
                 [
-                    InlineKeyboardButton(text="More Details", callback_data=f"flipkart | {more_details}")
+                    InlineKeyboardButton(text="More Details", callback_data=f"flipkart | {link2}")
                 ]
             ])
             answers.append(
