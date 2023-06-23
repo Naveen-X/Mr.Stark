54f3cf3d4b1b918fc26a1c80e2381ab76e2a1acb
Naveen_xD <naveenxd@devh.in>
2023-06-23 18:26:00 +0530

    Initial imports

---
+++ Stark/Plugins/stickers.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import time
+import math
+import random
+import logging
+from PIL import Image
+from datetime import datetime
+from random import randint as rain
+from os import environ, execle, path, remove
+
+
