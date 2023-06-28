076d51e7f5cd77319dea1cd06ca3e148dbbb0eb2
Naveen_xD <naveenxd@devh.in>
2023-06-28 15:04:12 +0530

    Add function

--- Stark/Plugins/gridpack.py
+++ Stark/Plugins/gridpack.py
@@ -7,3 +7,35 @@ from PIL import Image
 
 from Stark import error_handler
 from pyrogram import Client, filters
+
+
+#function to crop the image and divide it
+async def crop_and_divide(img):
+    (width, height) = img.size
+    rows = 5
+    columns = 5
+    scale_width = width // columns
+    scale_height = height // rows
+    if (scale_width * columns, scale_height * rows) != (width, height):
+        img = img.resize((scale_width * columns, scale_height * rows))
+    (new_width, new_height) = (0, 0)
+    media = []
+    for _ in range(1, rows + 1):
+        for o in range(1, columns + 1):
+            mimg = img.crop(
+                (
+                    new_width,
+                    new_height,
+                    new_width + scale_width,
+                    new_height + scale_height,
+                )
+            )
+            mimg = mimg.resize((512, 512))
+            image = io.BytesIO()
+            image.name = "Stark.png"
+            mimg.save(image, "PNG")
+            media.append(image.getvalue())
+            new_width += scale_width
+        new_width = 0
+        new_height += scale_height
+    return media
