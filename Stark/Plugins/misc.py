2e2e206920dca3332d2c053b651a21aa5ce2d7b8
Naveen_xD <naveenxd@devh.in>
2023-06-29 10:02:43 +0530

    update Stark/Plugins/misc.py

--- Stark/Plugins/misc.py
+++ Stark/Plugins/misc.py
@@ -1,7 +1,9 @@
-import pytz
+import os
+import urllib
 import random
 import asyncio
-import requests 
+import requests
+from telegraph import upload_file
 from pyrogram import Client, filters
 
 from Stark.db import DB
@@ -46,6 +48,51 @@ kakashitext = [
     "9",
 ]
 
+AVATARS = [
+     "https://telegra.ph//file/77c17cea26e8c24c36ea8.jpg",
+     "https://telegra.ph//file/e7f88921671418be96686.jpg",
+     "https://telegra.ph//file/10371646861fcfb521195.jpg",
+     "https://telegra.ph//file/1ab533ec778f56c4ecc5f.jpg",
+     "https://telegra.ph//file/75b0d1a20e8e9283c067b.jpg",
+     "https://telegra.ph//file/7939490055915bed0585c.jpg",
+     "https://telegra.ph//file/cd6dd4acae3c276c065be.jpg",
+     "https://telegra.ph//file/fced18b5d48c0cc3abb46.jpg",
+     "https://telegra.ph//file/79f1ccebc6aba285c6038.jpg",
+     "https://telegra.ph//file/a3423cefc893a6cd6032d.jpg",
+     "https://telegra.ph//file/2cc12ce620a035510b71d.jpg",
+     "https://telegra.ph//file/f40ec98eb82f48e301480.jpg",
+     "https://telegra.ph//file/e14a9c34b0bd6dab04333.jpg",
+     "https://telegra.ph//file/d782da8113a71bbd6e64c.jpg",
+     "https://telegra.ph//file/e3d4ac98f08cc3e1a5c74.jpg",
+     "https://telegra.ph//file/f5fe3e20d9ff18edf2cde.jpg",
+     "https://telegra.ph//file/156fb0e4fb7b16e1ad53f.jpg",
+     "https://telegra.ph//file/46997187cdadd13af1a4a.jpg",
+     "https://telegra.ph//file/e4484c6f3aafb531881fe.jpg",
+     "https://telegra.ph//file/8706beb96352012a02225.jpg",
+     "https://telegra.ph//file/42af4f48fa7684cd9709e.jpg",
+     "https://telegra.ph//file/b835311fae7dfbcb5a110.jpg",
+     "https://telegra.ph//file/13e782d79a69df17256ac.jpg",
+     "https://telegra.ph//file/556d9f06cedd634dc6e0b.jpg",
+     "https://telegra.ph//file/63b7803170528445cd2ea.jpg",
+     "https://telegra.ph//file/19364431c410895b30666.jpg",
+     "https://telegra.ph//file/02a9eec0c7d58a9a2d2da.jpg",
+     "https://telegra.ph//file/99c47bba3528fb3513bf7.jpg",
+     "https://telegra.ph//file/04b8c4fdcecc4f08a0337.jpg",
+     "https://telegra.ph//file/56fc54bdb387b21508dc4.jpg",
+     "https://telegra.ph//file/06f01066e22923b3fa07c.jpg",
+     "https://telegra.ph//file/3be8c6038f938679b287a.jpg",
+     "https://telegra.ph//file/c84d1903f515c2bd9a5a5.jpg",
+     "https://telegra.ph//file/f73ab4cb251dd5a1faeb6.jpg",
+     "https://telegra.ph//file/199fb4dfabb5a966e2cc5.png",
+     "https://telegra.ph//file/6c154cf21a9debdeb0f10.jpg",
+     "https://telegra.ph//file/199fb4dfabb5a966e2cc5.png",
+     "https://telegra.ph//file/9173ece9ac42e18bca59f.jpg",
+     "https://telegra.ph//file/34f4d70b52785b6c7d519.jpg",
+     "https://telegra.ph//file/9d197b2720c58bd7635d6.png",
+     "https://telegra.ph//file/cd793eb6485c251acb821.png",
+     "https://telegra.ph//file/bdf9039fd7adcf74c64ea.png",
+     "https://telegra.ph//file/b9770ec62a54fefd466b2.jpg",
+ ]
 
 kakashiemoji = [
     "⁭\n                    💖\n                  💖💖\n               💖💖💖\n            💖💖 💖💖\n          💖💖    💖💖\n        💖💖       💖💖\n      💖💖💖💖💖💖\n     💖💖💖💖💖💖💖\n   💖💖                 💖💖\n  💖💖                    💖💖\n💖💖                       💖💖\n",
@@ -341,6 +388,7 @@ async def c_emoji(client, message):
     await ok.edit(result)
 
 @Client.on_message(filters.command(["ftext", "f"]))
+@error_handler
 async def ftext(client, message):
     try:
         input_str = message.text.split(None, 1)[1]
@@ -368,3 +416,24 @@ async def ftext(client, message):
         await message.reply_to_message.reply_text(pay)
     else:
         await message.reply_text(pay)
+
+@Client.on_message(filters.command(["ytc"]))
+@error_handler
+async def yt_comment(client, message):
+   ytc = await message.reply_text("`Making Comment`")
+   try:
+        input_str = message.text.split(None, 1)[1]
+   except IndexError:
+        await ytc.edit("`Gib Some text to Make yt comment, U Dumb!`")
+        return
+   text = urllib.parse.quote_plus(input_str)
+   name = message.from_user.first_name
+   async for photo in client.get_chat_photos(message.from_user.id, limit=1):
+      img = photo 
+   pic = await client.download_media(img[0].file_id)
+   kk = upload_file(pic)
+   imglink = f"https://telegra.ph{kk[0]}"
+   lol = f"https://some-random-api.ml/canvas/youtube-comment?avatar={imglink}&username={name}&comment={text}"
+   await client.send_photo(message.chat.id, lol, caption=f"__**Made using @Mr_StarkBot**__")
+   os.remove(pic)
+   await ytc.delete()
