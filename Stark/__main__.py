76614f6936c66217f7e6f3e441fd92eb4c318b27
Naveen_xD <naveenxd@devh.in>
2023-07-06 19:03:13 +0530

    Not using anymore

--- Stark/__main__.py
+++ Stark/__main__.py
@@ -165,32 +165,32 @@ async def _1check_for_it(client, message):
         logging.exception(e)
 
 
-async def get_random_quote():
-    quotes_api_endpoint = "https://api.quotable.io/random"
-    response = requests.get(quotes_api_endpoint)
-    if response.status_code != 200:
-        return f"Error fetching quote ({response.status_code})"
-    data = response.json()
-    quote_text = data["content"]
-    quote_author = data["author"]
-    reply_text = f"__{quote_text}__\n\n- `{quote_author}`"
+# async def get_random_quote():
+#     quotes_api_endpoint = "https://api.quotable.io/random"
+#     response = requests.get(quotes_api_endpoint)
+#     if response.status_code != 200:
+#         return f"Error fetching quote ({response.status_code})"
+#     data = response.json()
+#     quote_text = data["content"]
+#     quote_author = data["author"]
+#     reply_text = f"__{quote_text}__\n\n- `{quote_author}`"
 
-    return reply_text
-
-
-def send_quote():
-    chat_ids = [x["chat_id"] for x in DB.qt.find({}, {"chat_id": 1})]
-    quote = asyncio.run(get_random_quote())
-    for chat_id in chat_ids:
-        app.send_message(chat_id=chat_id, text=quote)
+#     return reply_text
 
 
-logging.info("[ Mr.Stark ] | [ Scheduler] - Adding tasks")
-scheduler = BackgroundScheduler(timezone=pytz.timezone('Asia/Kolkata'))
-scheduler.add_job(send_quote, 'cron', hour=8, minute=0, second=0)
-scheduler.add_job(send_quote, 'cron', hour=18, minute=0, second=0)
-scheduler.start()
-logging.info("[ Mr.Stark ] | [ Scheduler] - Sucessfully added tasks and started the scheduler")
+# def send_quote():
+#     chat_ids = [x["chat_id"] for x in DB.qt.find({}, {"chat_id": 1})]
+#     quote = asyncio.run(get_random_quote())
+#     for chat_id in chat_ids:
+#         app.send_message(chat_id=chat_id, text=quote)
+
+
+# logging.info("[ Mr.Stark ] | [ Scheduler] - Adding tasks")
+# scheduler = BackgroundScheduler(timezone=pytz.timezone('Asia/Kolkata'))
+# scheduler.add_job(send_quote, 'cron', hour=8, minute=0, second=0)
+# scheduler.add_job(send_quote, 'cron', hour=18, minute=0, second=0)
+# scheduler.start()
+# logging.info("[ Mr.Stark ] | [ Scheduler] - Sucessfully added tasks and started the scheduler")
 logging.info("ᗩՏՏIՏTᗩᑎT ᕼᗩՏ ᗷᗴᗴᑎ ՏTᗩᖇTᗴᗪ ՏᑌᑕᑕᗴՏՏᖴᑌᒪᒪY")
 
 idle()
