f1849aa432d2670cbe0448e4222628e761eb8c18
Naveen_xD <naveenxd@devh.in>
2023-07-07 07:07:06 +0530

    I fked from termux

--- Stark/Plugins/filetools.py
+++ Stark/Plugins/filetools.py
@@ -56,8 +56,11 @@ def unzip_file(zip_path, extract_dir):
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(extract_dir)
         for file_info in zip_ref.infolist():
-            extracted_files.append(os.path.join(extract_dir, file_info.filename))
+            # Exclude directories from the extracted files
+            if not file_info.is_dir():
+                extracted_files.append(os.path.join(extract_dir, file_info.filename))
     return extracted_files
+
 
 @Client.on_message(filters.command(["unzip"]))
 @error_handler
@@ -71,15 +74,39 @@ async def unzip_files(c, m):
       await m.reply_text("`What should I Unzip?`")
       return
     if reply and reply.document:
-        document = m.reply_to_message.document
+        document = reply.document
         if document.mime_type == 'application/zip':
             c_time=time.time()
             target_dir = f"downloads/unzip/{m.from_user.id}"
+            try:
+               await c.send_message(m.from_user.id, "**Files will be sent here**")
+            except errors.PeerIdInvalid:
+                 await m.reply_text("**Start Me in Pm First**")
+                 return
+            except errors.UserIsBlocked:
+                 await m.reply_text("**Start Me in Pm First**")
+                 return
             dl = await m.reply_text("`Downloading file...`")
-            zip_file = await reply.dowmload(progress=progress, progress_args=(dl, c_time, "`Downloading File!`"))
+            zip_file = await reply.download(progress=progress, progress_args=(dl, c_time, "`Downloading File!`"))
+            await dl.edit("`Downloading Done!!\nNow Unzipping it...`")
             extracted_file_paths = unzip_file(zip_file, target_dir)
-            for i in extracted_file_paths:
-                 await m.reply_document(i)
+            await dl.edit(f"**Found {len(extracted_file_paths)} files**\n`Now Uploading...")
+            for index, file in enumerate(extracted_file_paths, 1):
+                 try:
+                   await c.send_document(m.from_user.id, file)
+                   await dl.edit(f"**Uploaded** `{index}/{len(extracted_file_paths)}`")
+                # except errors.PeerIdInvalid:
+                #     await dl.edit("**Start Me in Pm First**")
+                # except errors.UserIsBlocked:
+                #     await dl.edit("**Start Me in Pm First**")
+                 except errors.FloodWait as e:
+                     await asyncio.sleep(e.value)
+                     await m.reply_document(file)
+                 except:
+                     continue
+            await dl.edit("**All files have been sent to ur PM**")
+            shutil.rmtree(target_dir)
+            os.remove(zip_file)
         else:
             await m.reply_text("`The replied file is not a zip.`")
     else:
