a2012ba1d163291640f2ef9b5a6af5115635396a
Naveen_xD <naveenxd@devh.in>
2023-07-09 10:30:28 +0530

    update Stark/Plugins/afk.py

---
+++ Stark/Plugins/afk.py
@@ -0,0 +1,54 @@
+import time
+from Stark.db import DB
+from Stark import error_handler
+from pyrogram.types import Message
+from pyrogram.enums import ChatType
+from pyrogram import Client, filters
+
+
+afk = DB.afk
+
+
+async def add_afk(afk_status, afk_since, reason, user):
+    stark = afk.find_one({"afk", {
+        "status" : str(afk_status),
+        "since" : str(afk_since),
+        "reason" : str(reason),
+        "user" : int(user)
+    }}
+  )
+    if stark is None:
+        afk.insert_one({"afk", {
+        "status" : str(afk_status),
+        "since" : str(afk_since),
+        "reason" : str(reason),
+        "user" : int(user)
+    }}
+        )
+
+async def remove_afk(afk_status, afk_since, reason, user):
+    afk.delete_one({"afk", {
+        "status" : str(afk_status),
+        "since" : str(afk_since),
+        "reason" : str(reason),
+        "user" : int(user)
+    }}
+   )
+
+@Client.on_message(filters.command("afk"))
+async def afk(c, m):
+    afk_time = int(time.time())
+    id = m.from_user.id
+    try:
+      arg = m.text.split(None, 1)[1]
+    except IndexError:
+      arg = None
+    if not arg:
+        reason = None
+    else:
+        reason = arg
+    await add_afk(True, afk_time, reason, id)
+    if reason:
+        await m.reply_text(f"**Ok peeps AFK time**\n\nReason : __{reason}__")
+    else:
+        await m.reply_text("**Ok peeps AFK time**")
