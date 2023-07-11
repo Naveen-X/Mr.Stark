a334d0540f44973e1c8ab42dd01204730680cbd6
Naveen_xD <naveenxd@devh.in>
2023-07-11 07:46:55 +0530

    update Stark/Plugins/instadl.py

--- Stark/Plugins/instadl.py
+++ Stark/Plugins/instadl.py
@@ -196,16 +196,20 @@ async def idgl(c, m):
         msg = await m.reply_text("`Downloading...`")
         rdata = get(f"https://igdl.annihilatorrrr.tk/dl?key=igdlbot&url={url}").json()
         data = rdata["urls"]
-        ismediagroup = bool(len(data) > 1)
-        if not ismediagroup:
-                await m.reply_video(data[0], caption=rdata["caption"]) if "mp4" in data[0] else await m.reply_photo(data[0], caption=rdata["caption"])
-        else:
-              files = []
-              for ind, x in enumerate(data):
-                      if "mp4" in data[ind]:
-                         files.append(InputMediaVideo(x, caption=rdata["caption"] if ind == 0 else ""))
-                      else:
-                         files.append(InputMediaPhoto(x, caption=rdata["caption"] if ind == 0 else ""))
-
-              await c.send_media_group(m.chat.id, files)
-              await msg.delete()
+        try:
+              ismediagroup = bool(len(data) > 1)
+              if not ismediagroup:
+                      await m.reply_video(data[0], caption=rdata["caption"]) if "mp4" in data[0] else await m.reply_photo(data[0], caption=rdata["caption"])
+              else:
+                    files = []
+                    for ind, x in enumerate(data):
+                            if "mp4" in data[ind]:
+                               files.append(InputMediaVideo(x, caption=rdata["caption"] if ind == 0 else ""))
+                            else:
+                               files.append(InputMediaPhoto(x, caption=rdata["caption"] if ind == 0 else ""))
+      
+                    await c.send_media_group(m.chat.id, files)
+                    await msg.delete()
+        except:
+          for i in data:
+            await m.reply_video(i)
