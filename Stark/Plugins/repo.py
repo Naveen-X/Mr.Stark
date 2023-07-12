e8bf21aed9bf8873ec5deec9ad2939b01133b934
Naveen_xD <naveenxd@devh.in>
2023-07-12 19:22:21 +0530

    update Stark/Plugins/repo.py

---
+++ Stark/Plugins/repo.py
@@ -0,0 +1,14 @@
+import platform
+from pyrogram import __version__
+from pyrogram import Client, filters
+
+bot_version = "V2.0"
+
+@Client.on_message(filters.command("repo"))
+async def repo(c, m): 
+    REPO =  ("__**Hey, I'm using Mr.Stark😎🔥 **__\n")
+    REPO += ("__A Bot Made From Scratch Using Pyrogram.__\n\n")
+    REPO += (f"**✘ Stark version:** __{bot_version}__\n")
+    REPO += (f"**✘ PyroGram Version:** __{__version__}__\n")
+    REPO += (f"**✘ Python Version:** __{platform.python_version()}__\n")
+    await m.reply_text(REPO)
