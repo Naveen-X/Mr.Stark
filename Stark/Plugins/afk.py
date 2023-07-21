f015ea0e4eb3dc0da905936ff0535a9395c0d3b6
Naveen_xD <naveenxd@devh.in>
2023-07-21 07:04:13 +0530

    update Stark/Plugins/afk.py

--- Stark/Plugins/afk.py
+++ Stark/Plugins/afk.py
@@ -107,10 +107,7 @@ async def reply_to_afk(c, m):
                 start_offset = ent.offset
                 end_offset = ent.offset + ent.length
                 mention_text = m.text[start_offset:end_offset]
-                try:
-                  chat = await c.get_users(mention_text)
-                except:
-                  pass
+                chat = await c.get_users(mention_text)
                 if not chat:
                     continue
                 user_id = chat.id
