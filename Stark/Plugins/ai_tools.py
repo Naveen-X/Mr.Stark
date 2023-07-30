624aa61ce784898b6c1f4cd7644eafeb551a8469
Naveen_xD <naveenxd@devh.in>
2023-07-30 12:46:23 +0530

    Idk

--- Stark/Plugins/ai_tools.py
+++ Stark/Plugins/ai_tools.py
@@ -15,6 +15,7 @@ from pyrogram.types import InputMediaPhoto
 
 from Stark import error_handler
 from Stark.config import Config
+from SafoneAPI import SafoneAPI
 
 #Lexica Art thing ...
 class Lexica:
@@ -43,38 +44,7 @@ class Lexica:
 
         return result_str
 
-    def generate(self):
-        response = httpx.post("https://z.lexica.art/api/generator", headers={
-            "cookie": self.cookie
-        }, json={
-            "requestId": str(uuid.uuid4()),
-            "id": self._generate_random_string(20),
-            "prompt": self.query,
-            "negativePrompt": self.negativePrompt,
-            "guidanceScale": self.guidanceScale,
-            "width": 512 if self.portrait else 768,
-            "height": 768 if self.portrait else 512,
-            "enableHiresFix": False,
-            "model": "lexica-aperture-v2",
-            "generateSources": []
-        }, timeout=50
-        )
-
-        return [f"https://image.lexica.art/full_jpg/{ids['id']}" for ids in response.json()["images"]]
-
 #Generate gpt response...
-def generate_response(query: str):
-  url = "https://api.safone.me/chatgpt"
-  h = {
-    "message": query,
-    "version": 3,
-    "chat_mode": "assistant",
-    "dialog_messages": "[{\"bot\":\"\",\"user\":\"\"}]"
-  }
-  y = requests.post(url, h).json()
-  
-  output = y["message"]
-  return output
 
 @Client.on_message(filters.command(['gpt', 'askgpt', 'chatgpt']))
 @error_handler
@@ -88,10 +58,9 @@ async def chatgpt(c, m):
         return
     query = quote(query)
     await c.send_chat_action(m.chat.id, enums.ChatAction.TYPING)
-    try:
-      response = generate_response(query)
-    except JSONDecodeError:
-      response = "`ChatGPT Error`"
+    api = SafoneAPI()
+    resp = await api.chatgpt(query)
+    response = resp.message
     await c.send_message(m.chat.id, response, reply_to_message_id=m.id)
     await c.send_chat_action(m.chat.id, enums.ChatAction.CANCEL)
 
