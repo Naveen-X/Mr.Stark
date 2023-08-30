a41096f8bb26425267e157804dd4eb656ce19781
Indela Naveen <naveenxd@devh.in>
2023-08-30 16:59:56 +0530

    update Stark/Plugins/welcome.py

--- Stark/Plugins/welcome.py
+++ Stark/Plugins/welcome.py
@@ -4,6 +4,9 @@ from pyrogram.types import Message
 from Stark.db import DB
 from Stark import error_handler
 
+
+NO_WLCM = [x["_id"] for x in DB.wlcm.find({}, {"_id": 1})]
+
 async def rm_wlcm(id):
         DB.wlcm.insert_one({
         "_id" : id,
@@ -23,9 +26,12 @@ async def remove_wlcm(c, m):
 @Client.on_message(filters.new_chat_members)
 @error_handler
 async def welcome(bot, message: Message):
-    new_members = [f"{u.mention}" for u in message.new_chat_members]
-    text = f"{emoji.SPARKLES} Hey {message.new_chat_members[0].mention}\nWelcome to {message.chat.title}, Have a Nice Day :)"
-    await message.reply_text(text)
+    if m.chat.id not in NO_WLCM:
+      new_members = [f"{u.mention}" for u in message.new_chat_members]
+      text = f"{emoji.SPARKLES} Hey {message.new_chat_members[0].mention}\nWelcome to {message.chat.title}, Have a Nice Day :)"
+      await message.reply_text(text)
+    else:
+      await m.reply_text("oke")
 
 
 @Client.on_message(filters.left_chat_member)
