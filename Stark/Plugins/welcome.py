916c085310627bab6ec1ff8c309e56fe82ac5118
Indela Naveen <naveenxd@devh.in>
2023-08-31 05:21:13 +0530

    update Stark/Plugins/welcome.py

--- Stark/Plugins/welcome.py
+++ Stark/Plugins/welcome.py
@@ -26,16 +26,15 @@ async def remove_wlcm(c, m):
 @Client.on_message(filters.new_chat_members)
 @error_handler
 async def welcome(bot, message: Message):
-    if m.chat.id not in NO_WLCM:
+    if message.chat.id not in NO_WLCM:
       new_members = [f"{u.mention}" for u in message.new_chat_members]
       text = f"{emoji.SPARKLES} Hey {message.new_chat_members[0].mention}\nWelcome to {message.chat.title}, Have a Nice Day :)"
       await message.reply_text(text)
-    else:
-      await m.reply_text("oke")
 
 
 @Client.on_message(filters.left_chat_member)
 @error_handler
 async def user_left(bot, message: Message):
-    text = "K bye!\nNice knowing you:("
-    await message.reply_text(text)
+    if message.chat.id not in NO_WLCM:
+      text = "K bye!\nNice knowing you:("
+      await message.reply_text(text)
