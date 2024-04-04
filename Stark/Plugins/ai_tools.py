8badce40f79ad4ddfc349d2f42552baa159231b4
I. Naveen <naveenxd@devh.in>
2024-04-04 21:02:55 +0530

    .

--- Stark/Plugins/ai_tools.py
+++ Stark/Plugins/ai_tools.py
@@ -24,49 +24,49 @@ api_key = Config.VSN_CRAFT
 #gpt base codes
 genai.configure(api_key=Config.GOOGLE_AI_STUDIO_KEY)
 
-    generation_config = {
-        "temperature": 0.9,
-        "top_p": 1,
-        "top_k": 1,
-        "max_output_tokens": 2048,
-    }
+generation_config = {
+    "temperature": 0.9,
+    "top_p": 1,
+    "top_k": 1,
+    "max_output_tokens": 2048,
+}
 
-    safety_settings = [
-        {
-            "category": "HARM_CATEGORY_HARASSMENT",
-            "threshold": "BLOCK_ONLY_HIGH"
-        },
-        {
-            "category": "HARM_CATEGORY_HATE_SPEECH",
-            "threshold": "BLOCK_ONLY_HIGH"
-        },
-        {
-            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-            "threshold": "BLOCK_ONLY_HIGH"
-        },
-        {
-            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-            "threshold": "BLOCK_ONLY_HIGH"
-        },
-    ]
+safety_settings = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_ONLY_HIGH"
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_ONLY_HIGH"
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_ONLY_HIGH"
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_ONLY_HIGH"
+    },
+]
 
-    model = genai.GenerativeModel(model_name="gemini-1.0-pro",
-                                  generation_config=generation_config,
-                                  safety_settings=safety_settings)
+model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
 
-    prompt_parts = [
-        "input: Who is your owner?",
-        "output: My owner is Naveen_xD",
-        "input: Are you lying?",
-        "output: No!",
-        "input: Are you trained by google",
-        "output: No",
-        "input: Who are you?",
-        "output: I am a bot developed by Naveen_xD",
-        "input: Are you trained by google",
-        "output: No, I am not trained by Google. I am trained by Naveen.",
-    ]
-    chat = []
+prompt_parts = [
+    "input: Who is your owner?",
+    "output: My owner is Naveen_xD",
+    "input: Are you lying?",
+    "output: No!",
+    "input: Are you trained by google",
+    "output: No",
+    "input: Who are you?",
+    "output: I am a bot developed by Naveen_xD",
+    "input: Are you trained by google",
+    "output: No, I am not trained by Google. I am trained by Naveen.",
+]
+chat = []
     
 #Lexica Art thing ...
 class Lexica:
