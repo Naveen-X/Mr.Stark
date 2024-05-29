4b0f700e31a92561447d0e7c1afd3af39a28ac68
Naveen_xD <naveenxd@devh.in>
2024-05-29 18:50:00 +0530

    Update config.py

--- Stark/config.py
+++ Stark/config.py
@@ -20,3 +20,7 @@ class Config(object):
     VSN_CRAFT = os.environ.get("VSN_CRAFT", None)
     CHROME_BIN = os.environ.get("CHROME_BIN",  "/usr/bin/google-chrome-stable")
     GOOGLE_AI_STUDIO_KEY = os.environ.get("GOOGLE_AI_STUDIO_KEY", None)
+    G_1 = os.environ.get("G_1", None)
+    G_2 = os.environ.get("G_2", None)
+    G_4 = os.environ.get("G_3", None)
+    G_4 = os.environ.get("G_4", None)
